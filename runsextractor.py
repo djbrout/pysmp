afa7e1f7ada54f6acda522c5746e291cf3074f68
djbrout <djbrout@gmail.com>
2016-05-25 17:58:43 -0400

    fixed imagefilename bug

--- runsextractor.py
+++ runsextractor.py
@@ -17,7 +17,7 @@ import subprocess
 
 def getsky_and_skyerr(imagefilename):
 
-    proc = subprocess.Popen("sex imagefilename -c defaults/default.sex", stdout=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen("sex "+imagefilename+" -c defaults/default.sex", stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     print "program output:", out
 
