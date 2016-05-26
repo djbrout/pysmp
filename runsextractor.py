46e5c09ec118970f3b34cb99bbdbcba59f966942
djbrout <djbrout@gmail.com>
2016-05-26 08:58:53 -0400

    trying sewpy

--- runsextractor.py
+++ runsextractor.py
@@ -14,23 +14,33 @@ values from any image
 
 #import os
 import subprocess
+import sewpy
+import logging
+
 
 def getsky_and_skyerr(imagefilename):
 
-    proc = subprocess.Popen("sex "+imagefilename+" -c defaults/default.sex", stdout=subprocess.PIPE, shell=True)
+    #proc = subprocess.Popen("sex "+imagefilename+" -c defaults/default.sex", stdout=subprocess.PIPE, shell=True)
     #(out, err) = proc.communicate()
     #print "program output:", out
     #proc = subprocess.Popen(['python', 'fake_utility.py'], stdout=subprocess.PIPE)
-    while True:
-        line = proc.stdout.readline()
-        if line != '':
-            # the real code does filtering here
-            print "test:", line.rstrip()
-        else:
-            break
+    #while True:
+    #    line = proc.stdout.readline()
+    #    if line != '':
+    #        # the real code does filtering here
+    #        print "test:", line.rstrip()
+    #    else:
+    #        break
 
-            #os.system('sex '+imagefilename+' -c defaults/default.sex')
+    #        #os.system('sex '+imagefilename+' -c defaults/default.sex')
 
+
+    logging.basicConfig(format='%(levelname)s: %(name)s(%(funcName)s): %(message)s', level=logging.DEBUG)
+    sew = sewpy.SEW(
+            sexpath="sex"
+        )
+    out = sew(imagefilename)
+    print out.keys()
 
 im = '/global/cscratch1/sd/dbrout/v3/20130902_SN-S2/r_21/SNp1_230168_SN-S2_tile20_r_21.fits'
 getsky_and_skyerr(im)
