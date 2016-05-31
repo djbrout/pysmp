c7cec0b0c90d78703335c00d638735c9e1ccc0c3
djbrout <djbrout@gmail.com>
2016-05-31 11:58:36 -0400

    rudimentary checkstar analysis is running. added rdcol to dilltools

--- checkstar_analysis.py
+++ checkstar_analysis.py
@@ -1,1 +1,23 @@
-__author__ = 'djbrout'
+import os
+import matplotlib as m
+
+m.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.set_printoptions(threshold=np.nan)
+import sys
+import dilltools as dt
+
+default_checkstar_file = '/Volumes/ExtraSpace/pysmp_downloads/des_fake_00229567_r_standardstarfits.txt'
+
+
+def checkstars(checkstarfile=default_checkstar_file):
+    cols = dt.readcol(checkstarfile,delim='\t')
+
+    print cols.keys()
+    return cols
+
+
+if __name__ == '__main__':
+    a = checkstars()
--- dilltools.py
+++ dilltools.py
@@ -119,3 +119,41 @@ def psfphotometry(im, psf, sky, weight, gal, guess_scale):
     return fluxvec[chisqvec == min(chisqvec)], fluxvec[chisqvec == min(chisqvec)] - fluxvec[idx][
         0], mchisq / ndof, sum_data_minus_sim
 
+
+# Takes in Filename, reads file columnwise, and returns dictionary such that:
+# import rdcol
+# a = rdcol.read('filename',headline,datastartline)
+# a["Column_Name"] -> returns the list for that column
+#
+# headline and datastartline are always > 0
+#
+# By Dillon Brout
+# dbrout@physics.upenn.edu
+
+def readcol(filename,headline=1,startline=2,delim=' '):
+    linenum = 0
+    go = 0
+    column_list = []
+    return_cols = {}
+    inf = open(filename)
+    for line in inf:
+        line = line.replace('#', '')
+        line = line.strip()
+        cols = line.split(delim)
+        cols[:] = (value for value in cols if value != '')
+        if linenum == headline - 1:
+            for col in cols:
+                return_cols[col.strip()] = []
+                column_list.append(col.strip())
+                go += 1
+        if linenum >= startline - 1:
+            index = 0
+            for col in cols:
+                try:
+                    return_cols[column_list[index]].append(float(col.strip()))
+                except:
+                    return_cols[column_list[index]].append(col.strip())
+                index += 1
+        linenum += 1
+    inf.close()
+    return return_cols
