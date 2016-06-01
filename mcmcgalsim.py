609abd32dcd737539205b596880d4c7e18d86182
djbrout <djbrout@gmail.com>
2016-06-01 16:14:35 -0400

    updated mcmcgalsim2

--- mcmcgalsim.py
+++ mcmcgalsim.py
@@ -160,7 +160,7 @@ class metropolis_hastings():
 
         #self.pixelation_factor = model_pixel_scale/platescale
         #self.galaxy_model = self.pixelate(galmodel,self.pixelation_factor)
-        self.galaxy_model = np.zeros(galmodel.shape) + np.mean(galmodel)
+        self.galaxy_model = np.zeros((fitradius*2+1,fitradius*2+1)) + np.mean(galmodel)
         self.kicked_galaxy_model = copy(self.galaxy_model)
         self.kicked_galmodel = copy(self.galaxy_model)
         #print 'gmshape',self.galaxy_model.shape
