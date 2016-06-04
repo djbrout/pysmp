2ef23d2b6e689290d31bd5495846614fbf0daedc
djbrout <djbrout@gmail.com>
2016-06-04 10:12:44 -0400

    min 120k steps

--- bigplots.py
+++ bigplots.py
@@ -337,7 +337,7 @@ for sn in np.arange(len(snnames)):
 	mm = np.argmax(withsn['modelvec'])
 	wsn_val = []
 	wsnh = withsn['modelvec_nphistory'][:,mm]
-	if len(wsnh) < 700:
+	if len(wsnh) < 1200:
 		print snnames[sn]+' Not enough steps for convergence '+str(len(wsnh))+'... Skipping...'
 		#raw_input()
 		continue
