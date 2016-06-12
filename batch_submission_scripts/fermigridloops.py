26a4433746c288c9c09ac1c10d741c70bce37db3
djbrout <djbrout@gmail.com>
2016-06-12 17:15:18 -0400

    Delete fermigridloops.py

--- batch_submission_scripts/fermigridloops.py
+++
@@ -1,4 +0,0 @@
-import os
-for i in range(0,2):
-	os.system('jobsub_submit -G des --resource-provides=usage_model=DEDICATED -M --OS=SL6 file://batch_submission_scripts/runfermirun.csh '+str(i)+' g')
-	print i,'submitted'
