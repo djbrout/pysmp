29877013e49e396644ddf5c36da603ac776d3bab
Dillon Brout <djbrout@gmail.com>
2016-06-13 07:08:05 -0500

    up to date

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -2,8 +2,8 @@ source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
 . /grid/fermiapp/products/common/etc/setups.sh
 setup jobsub_client
 
-for i in {0..2}
+for i in {0..0}
 do
-    jobsub_submit -G des --resource-provides=usage_model=DEDICATED -M --OS=SL6 file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh $i g   
+    jobsub_submit -G des --resource-provides=usage_model=DEDICATED -M --verbose --OS=SL6 file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh $i g --log_file=/pnfs/des/scratch/pysmp/logfiles/log_$i.log  
     echo job $i submitted
 done
