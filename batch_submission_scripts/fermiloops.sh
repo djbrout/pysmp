1efde39e0886a570bbb1045dbfe3dc12a9dbbc1a
djbrout <djbrout@gmail.com>
2016-06-13 08:21:24 -0400

    trying log again

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -6,7 +6,7 @@ for i in {0..0}
 do
     jobsub_submit -G des --resource-provides=usage_model=DEDICATED -M\
      --verbose --OS=SL6 file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh $i g\
-      --log_file=/pnfs/des/scratch/pysmp/logfiles/log_$i.log\
+      --log_file=/data/des41.a/data/djbrout/pysmp/logs/log_$i.log\
       --generate-email-summary
     echo job $i submitted
 done
