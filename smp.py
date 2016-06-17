5a42eda85a87b2991b9f435df8519d1f2f96713d
djbrout <djbrout@gmail.com>
2016-06-17 09:48:37 -0400

    psfcentering

--- smp.py
+++ smp.py
@@ -3371,7 +3371,7 @@ class smp:
                         psf, psfcenter = self.psf, self.psfcenter
                     else:
                         #print 'xyxyxyxy'
-                        psf, psfcenter = self.psf, (round(x),round(y))
+                        psf, psfcenter = self.psf, (x,y)
                         #print psfcenter
                 
                 counter += 1
