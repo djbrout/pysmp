8653fea8be2b4c4293315866b28b1d97a2aa0821
Dillon Brout <djbrout@gmail.com>
2016-06-22 08:53:10 -0500

    updated jobsub scripts

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -4,11 +4,11 @@ source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
 
 setup jobsub_client
 
-for i in {0..0}
-do
-    jobsub_submit -G des --resource-provides=usage_model=DEDICATED -M\
+num_jobs = 1
+
+jobsub_submit -G des --resource-provides=usage_model=DEDICATED -M\
      --verbose --OS=SL6 file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh $i g\
       --log_file=/data/des41.a/data/djbrout/pysmp/logs/log_$i.log\
-      --generate-email-summary
-    echo job $i submitted
+      --generate-email-summary -N $num_jobs
+echo $num_jobs jobs submitted
 done
--- batch_submission_scripts/runfermirun.csh
+++ batch_submission_scripts/runfermirun.csh
@@ -24,4 +24,4 @@ cd pysmp
 pwd
 echo 'running smp.py!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 
-python smp.py --index=0 -f g --nozpt
+python smp.py --index=0 -f g --nozpt --fermigrid
