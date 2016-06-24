72c04a6663c2db95a86c1451cfed90f55c6de4bf
djbrout <djbrout@gmail.com>
2016-06-23 20:04:16 -0400

    substamp

--- smp.py
+++ smp.py
@@ -3438,7 +3438,7 @@ class smp:
                 try:
                     errmag, chi, niter, scale, iylo, iyhi, ixlo, ixhi, image_stamp, noise_stamp, mask_stamp, psf_stamp = \
                         pk.pkfit_norecent_noise_smp(1, x, y, s, se, params.fitrad, returnStamps=True,
-                                                    stampsize=params.substamp-1.)
+                                                    stampsize=params.substamp)
 
                     noise_stamp[noise_stamp > 0.] = 1
                     noise_stamp[noise_stamp <= 0.] = 0
