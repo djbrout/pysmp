52591b061e16656bb79158815350a38695d325e1
djbrout <djbrout@gmail.com>
2016-07-06 09:43:42 -0400

    setups2

--- setup_scripts/setup_fermilab_des41.a.sh
+++ setup_scripts/setup_fermilab_des41.a.sh
@@ -1,21 +1,15 @@
 
+
+
 source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup jobsub_client
 kx509
 voms-proxy-init -rfc -noregen -valid 24:00 -voms des:/des/Role=DESGW
 
-#alias sqs="jobsub_q -G des --role=DESGW --user=${USER}"
+#alias sqs="jobsub_q -G des --role=DESGW --user=desgw"
 
-
-#source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
-## source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
-## setup jobsub_client
-##kx509
-##voms-proxy-init -rfc -noregen -valid 24:00 -voms des:/des/Role=DESGW
-#
-##alias sqs="jobsub_q -G des --role=DESGW --user=desgw"
-#
-## setup perl 5.18.1+6 || exit 134
+# setup perl 5.18.1+6 || exit 134
 setup Y2Nstack 1.0.6+18
 setup diffimg gwdevel8
 setup ftools v6.17
@@ -24,6 +18,6 @@ setup easyaccess
 setup extralibs 1.0
 echo "EUPS setup complete"
 
-#source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
-#setup jobsub_client
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+setup jobsub_client
 echo 'done setting up jobsub_client'
