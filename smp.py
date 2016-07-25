c50871452116650c9db8adbaf75e3aee1246fd2d
djbrout <djbrout@gmail.com>
2016-07-25 15:47:12 -0400

    removed cntr

--- smp.py
+++ smp.py
@@ -3450,7 +3450,7 @@ class smp:
         #sys.exit()
         for x,y,m,s,se,mc,ra,dec,i in zip(xstar,ystar,mags,sky,skyerr,mag_cat,ras,decs,range(len(xstar))):
             cntr += 1
-            if cntr > 100:
+            if cntr > 50:
                 continue
             #print 'xstar',xstar
             #raw_input()
