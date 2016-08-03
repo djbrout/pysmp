bade811e45008623cd29816a614ac0004a4714bc
djbrout <djbrout@gmail.com>
2016-08-03 15:14:33 -0400

    debugging smp_im

--- smp.py
+++ smp.py
@@ -812,7 +812,7 @@ class smp:
                 zip(snparams.image_name_search,snparams.image_name_weight,snparams.file_name_psf,snparams.band,snparams.fake_truemag, range(len(snparams.band))):
             if snparams.mjd[j] == 0:
                 continue
-            if cccc > 4:
+            if cccc > 40000:
                 continue
             #print imfile
             #raw_input()
