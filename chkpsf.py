9e75c9fbe0f0517dcd180da93f2eba0da0fd5c9b
djbrout <djbrout@gmail.com>
2016-08-03 14:03:52 -0400

    debugging smp_im

--- chkpsf.py
+++ chkpsf.py
@@ -184,10 +184,11 @@ def fit(
         if thisclass[0] == 1:
             good = True
 
-    print 'fluxls',fluxls
-    print 'maxpsf',np.max(rpsf)
+
     if returnstamps:
         rpsf = model
         good = True#we know this wont be in the starcat file so set to good is true
+        print 'fluxls', fluxls
+        print 'maxpsf', np.max(rpsf)
         return fluxls,fluxerr,chisq,dms,good,subim, rpsf, imhdr['SKYSIG'], fitrad, imhdr['SKYADU'], hpsf['PSFMAG'], submask
     return fluxls, fluxerr, chisq, dms, good
