2ab36f054e9b4322afa6cfcca40e0e8228b2eea3
djbrout <djbrout@gmail.com>
2016-08-27 23:39:07 -0400

    testing no globalstar moved indexing into ps1 and not getzpt

--- smp.py
+++ smp.py
@@ -3734,7 +3734,7 @@ class smp:
                     #print 'initialized'
                     try:
                         errmag, chi, niter, scale, iylo, iyhi, ixlo, ixhi, image_stamp, noise_stamp, mask_stamp, psf_stamp = \
-                            pk.pkfit_norecent_noise_smp(1, x-1., y, s, se, params.fitrad, returnStamps=True,
+                            pk.pkfit_norecent_noise_smp(1, x, y, s, se, params.fitrad, returnStamps=True,
                                                         stampsize=params.substamp)
                         #print 'scale CHECKEEEEEE', scale, scaleck
 
@@ -3786,25 +3786,25 @@ class smp:
                                                                          gal, mjd, scale, index=i)
 
                         print 'index',i,'chisq',chisq,'xpix',x,'ypix',y,'xlow',ixlo,'xhi',ixhi,'ylow',iylo,'yhi',iyhi,
-                        #if y-np.floor(y) < 5.:
-                        suby = 17
-                        addy = 18
-                        #else:
-                        #    suby= 16
-                        #    addy = 18
-
-                        #if x - np.floor(x) < 5.:
-                        subx = 17
-                        addx = 18
-                        #else:
-                        #    subx = 16
-                        #    addx = 18
-                        mimage_stamp = im[np.round(y)-suby:np.round(y)+addy,np.round(x-1.)-subx:np.round(x-1.)+addx]
-                        mcscale, mcscale_std, mchisq, mdms = self.getfluxsmp(mimage_stamp, psf_stamp, sexsky, noise_stamp,
-                                                                         params.fitrad,
-                                                                         gal, mjd, scale, index=i+1000)
-                        #print 'checking!!!', cscale, oldcscale
-                        print 'DIFFFFFF',scale,cscale,mcscale
+                        # #if y-np.floor(y) < 5.:
+                        # suby = 17
+                        # addy = 18
+                        # #else:
+                        # #    suby= 16
+                        # #    addy = 18
+                        #
+                        # #if x - np.floor(x) < 5.:
+                        # subx = 17
+                        # addx = 18
+                        # #else:
+                        # #    subx = 16
+                        # #    addx = 18
+                        # mimage_stamp = im[np.round(y)-suby:np.round(y)+addy,np.round(x)-subx:np.round(x)+addx]
+                        # mcscale, mcscale_std, mchisq, mdms = self.getfluxsmp(mimage_stamp, psf_stamp, sexsky, noise_stamp,
+                        #                                                  params.fitrad,
+                        #                                                  gal, mjd, scale, index=i+1000)
+                        # #print 'checking!!!', cscale, oldcscale
+                        # print 'DIFFFFFF',scale,cscale,mcscale
                         self.tmpwriter.savefits(image_stamp-mimage_stamp,'/pnfs/des/scratch/pysmp/test/'+str(i)+'_pk-me.fits')
                         #sys.exit()
                         #scale = cscale
