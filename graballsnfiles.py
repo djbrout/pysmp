2ef8532297c2ff6dd575feed731275739dfe6400
djbrout <djbrout@gmail.com>
2016-09-10 13:15:31 -0400

    grabbing again

--- graballsnfiles.py
+++ graballsnfiles.py
@@ -12,12 +12,12 @@ filename = 'SN-S1_CCD01_v3.tar'
 #'kx509; grid-proxy-init; voms-proxy-init -rfc -noregen -voms des:/des/Role=Analysis; '
 
 for filename in files:
-
-    if os.path.isfile("/pnfs/des/scratch/pysmp/v3"+filename):
-        if os.stat("/pnfs/des/scratch/pysmp/v3"+filename).st_size > 0.:
+    #print filename
+    if os.path.isfile("/pnfs/des/scratch/pysmp/v3/"+filename):
+        if os.stat("/pnfs/des/scratch/pysmp/v3/"+filename).st_size > 0.:
             print filename,'already exists'
             continue
-
+    print 'globus copying',filename
     out = os.popen( 'globus-url-copy -nodcau -cred /tmp/x509up_u48121 '
                 '-ss "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=dtn03-garchive.nersc.gov"'
                 ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v3/' +
