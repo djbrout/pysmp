b4331ff4dd23b0f31e27f4f2491a9402110d2369
djbrout <djbrout@gmail.com>
2016-09-09 22:06:01 -0400

    testing file transfer from hpss to fermigrid

--- graballsnfiles.py
+++ graballsnfiles.py
@@ -3,7 +3,7 @@ import os
 filename = 'SN-S1_CCD01_v3.tar'
 'kx509; grid-proxy-init; voms-proxy-init -rfc -noregen -voms des:/des/Role=Analysis; '
 out = os.popen( 'globus-url-copy -nodcau -cred /tmp/x509up_u48121 '
-                '-ss "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=cori17-224.nersc.gov"'
+                '-ss "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=dtn03-garchive.nersc.gov"'
                 ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v3/' +
                 filename+' gsiftp://fndca1.fnal.gov:2811/des/scratch/pysmp/v3/'+filename).read()
 print out
