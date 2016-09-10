e99b494cad1264ce9e2007c228688331522bb264
djbrout <djbrout@gmail.com>
2016-09-09 23:08:54 -0400

    testing file transfer from hpss to fermigrid

--- graballsnfiles.py
+++ graballsnfiles.py
@@ -2,14 +2,22 @@ import os
 import sys
 
 f = open('sntarfilesv3.txt','r').read()
-print f.split()
+files = f.split()
 
-sys.exit()
+#sys.exit()
 
 filename = 'SN-S1_CCD01_v3.tar'
-'kx509; grid-proxy-init; voms-proxy-init -rfc -noregen -voms des:/des/Role=Analysis; '
-out = os.popen( 'globus-url-copy -nodcau -cred /tmp/x509up_u48121 '
+
+#NEED TO DO THIS EVERY DAY
+#'kx509; grid-proxy-init; voms-proxy-init -rfc -noregen -voms des:/des/Role=Analysis; '
+
+for filename in files:
+
+    out = os.popen( 'globus-url-copy -nodcau -cred /tmp/x509up_u48121 '
                 '-ss "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=dtn03-garchive.nersc.gov"'
                 ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v3/' +
                 filename+' gsiftp://fndca1.fnal.gov:2811/des/scratch/pysmp/v3/'+filename).read()
-print out
+    print out
+    print filename
+
+    
