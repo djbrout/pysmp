620773cd0b6dd5fccc5ed29da1554a9a2e5e1822
djbrout <djbrout@gmail.com>
2016-09-21 13:48:02 -0400

    swapped x and y in build psf call

--- smp.py
+++ smp.py
@@ -1829,7 +1829,7 @@ class smp:
                     #x_star1, y_star1 = cntrd.cntrd(im, x_star1, y_star1, params.cntrd_fwhm*2.)
                     #print x_star1[0]-x_star1n[0]
                     #raw_input('stopped')
-                    zpt,zpterr,zpt_file = self.getzpt(x_star1,y_star1,tras,tdecs,starcat,mag,sky,skyerr,snparams.mjd[j],
+                    zpt,zpterr,zpt_file = self.getzpt(x_starold,y_starold,tras,tdecs,starcat,mag,sky,skyerr,snparams.mjd[j],
                                          badflagx,mag_star,im,weights,mask,maskfile,psffile,imfile,snparams,params.substamp,mjdoff,mjdslopeinteroff,j,
                                          longimfile,psf=self.psf,mjd=str(float(snparams.mjd[j])))
                     # zpt, zpterr, zpt_file = self.getzpt(x_starold, y_starold, starcat.ra[cols], starcat.dec[cols], starcat, mag, sky, skyerr,
