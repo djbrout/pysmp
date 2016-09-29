7b42c2d799069afa73750493ae0c60c9569cad5c
djbrout <djbrout@gmail.com>
2016-09-29 14:24:04 -0400

    close to big run, need to fix output diretories for mcmc

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -6,6 +6,7 @@ setup jobsub_client
 
 
 jobsub_submit -G des --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC -M --verbose --OS=SL6\
- file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh --log_file=/pnfs/des/persistent/smp/logs/r1.log --generate-email-summary -N 2
+  --log_file=/pnfs/des/persistent/smp/logs/r1.log --generate-email-summary -N 2\
+  file://runfermirun.csh
 echo 2 jobs submitted
 echo done
