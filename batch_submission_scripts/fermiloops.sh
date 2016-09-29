edb21b2c1edd485dd7de483ede637fb21956854c
djbrout <djbrout@gmail.com>
2016-09-29 11:31:39 -0400

    close to big run, need to fix output diretories for mcmc

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -5,7 +5,7 @@ source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
 setup jobsub_client
 
 
-jobsub_submit -G des --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC -M --verbose --OS=SL6 --log_file=/pnfs/des/persistent/smp/logs/r1.log --generate-email-summary -N 2 --memory=1.9GB file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh -N 2
+jobsub_submit -G des --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC -M --verbose --OS=SL6 --log_file=/pnfs/des/persistent/smp/logs/r1.log --generate-email-summary --memory=1.9GB file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh -N 2
 
 
 echo 2 jobs submitted
