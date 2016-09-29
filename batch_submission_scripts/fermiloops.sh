4b4010ff0b0b131ff06b378c1d917fa1feeabb21
djbrout <djbrout@gmail.com>
2016-09-28 23:45:09 -0400

    test process

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -5,9 +5,6 @@ source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
 setup jobsub_client
 
 
-jobsub_submit -G des --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC -M\
-     --verbose --OS=SL6 file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh\
-      --log_file=/pnfs/des/persistent/smp/logs/r1.log\
-      --generate-email-summary -N 2 --memory=5.9GB
+jobsub_submit -G des --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC -M --verbose --OS=SL6 file:///data/des41.a/data/djbrout/pysmp/batch_submission_scripts/runfermirun.csh --log_file=/pnfs/des/persistent/smp/logs/r1.log --generate-email-summary -N 2 --memory=5.9GB
 echo 2 jobs submitted
 echo done
