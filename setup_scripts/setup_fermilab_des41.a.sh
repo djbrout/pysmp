b13e9bb25477f1123f314b05655678c55742ff59
djbrout <djbrout@gmail.com>
2016-10-02 15:31:10 -0400

    changed cvmfs setup script

--- setup_scripts/setup_fermilab_des41.a.sh
+++ setup_scripts/setup_fermilab_des41.a.sh
@@ -1,9 +1,9 @@
 
 
 
-#source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
-source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
-setup jobsub_client
+source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
+#source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+#setup jobsub_client
 kx509
 voms-proxy-init -rfc -noregen -valid 24:00 -voms des:/des/Role=Analysis
 
@@ -19,6 +19,6 @@ setup extralibs 1.0
 setup ifdhc
 echo "EUPS setup complete"
 
-#source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
-#setup jobsub_client
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+setup jobsub_client
 echo 'done setting up jobsub_client'
