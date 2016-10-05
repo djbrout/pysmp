90f59c38bbd744bf1ae8fee53c4503978b995e6a
djbrout <djbrout@gmail.com>
2016-10-05 11:00:08 -0400

    added new file

--- dilltools.py
+++ dilltools.py
@@ -161,31 +161,18 @@ def readcol(filename,headline=1,startline=2,delim=' '):
                 go += 1
         if linenum >= startline - 1:
             index = 0
-            # if len(cols) != numcols:
-            #     print 'WARNING: Could not read line ' + str(linenum + 1) + ' of ' + filename
-            # else:
-            passed = True
-
-            for col in cols:
-                try:
-                    print len(return_cols[column_list[index]])
-                except:
-                    try:
-                        print len(return_cols[column_list[index]])
-                    except:
-                        print 'WARNING: Could not read line ' + str(linenum + 1) + ' of ' + filename
-                        passed = False
-                index += 1
-            index = 0
-
-            if passed:
+            if len(cols) != numcols:
+                 print 'WARNING: Could not read line ' + str(linenum + 1) + ' of ' + filename
+            else:
                 for col in cols:
                     try:
                         return_cols[column_list[index]].append(float(col.strip()))
                     except:
-                        return_cols[column_list[index]].append(col.strip())
+                        try:
+                            return_cols[column_list[index]].append(col.strip())
+                        except:
+                            print 'WARNING: Could not read line '+str(linenum+1)+' of '+filename
                     index += 1
-
         linenum += 1
     inf.close()
     for k in return_cols.keys():
