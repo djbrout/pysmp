d46e0b08e4fecbbd659018c27f6c1a202cd3ea1e
djbrout <djbrout@gmail.com>
2016-10-05 09:46:59 -0400

    added new file

---
+++ summarizebigrun.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+import exceptions
+import os
+import sys
+import matplotlib as m
+m.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+import pyfits as pf
+import scipy.signal
+from copy import copy
+import time
+from astropy.io import fits
+from scipy.interpolate import UnivariateSpline
+import sigma_clip
+import meanclip
+import dilltools as dt
+
+
+resultsdir = '/pnfs/des/scratch/pysmp/smp_02/'
+
+def go(resultsdir):
+    files = os.listdir(os.path.join(resultsdir,'lightcurves'))
+    smpfiles = []
+    for f in files:
+        if '.smp' in f:
+            smpfiles.append(f)
+            print f
+
+if __name__ == "__main__":
+    go(resultsdir)
