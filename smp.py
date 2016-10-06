b786df48efeb949b0f5c7b34d0fc572c66043d94
djbrout <djbrout@gmail.com>
2016-10-06 13:10:22 -0400

    testing stars

--- smp.py
+++ smp.py
@@ -1915,11 +1915,11 @@ class smp:
                 if not nozpt:
                     skipactualzeropoint = True
                 if not skipactualzeropoint:
-                    #x_star1, y_star1 = cntrd.cntrd(im, x_star1, y_star1, params.cntrd_fwhm)
+                    x_star1, y_star1 = cntrd.cntrd(im, x_star1, y_star1, params.cntrd_fwhm)
                     #print x_star1[0]-x_star1n[0]
                     #raw_input('stopped')
                     #sys.exit()
-                    zpt,zpterr,zpt_file = self.getzpt(x_starold,y_starold,tras,tdecs,starcat,mag,sky,skyerr,snparams.mjd[j],
+                    zpt,zpterr,zpt_file = self.getzpt(x_star1,y_star1,tras,tdecs,starcat,mag,sky,skyerr,snparams.mjd[j],
                                          badflagx,mag_star,im,weights,mask,maskfile,psffile,imfile,snparams,params.substamp,mjdoff,mjdslopeinteroff,j,
                                          longimfile,psf=self.psf,mjd=str(float(snparams.mjd[j])))
                     # zpt, zpterr, zpt_file = self.getzpt(x_starold, y_starold, starcat.ra[cols], starcat.dec[cols], starcat, mag, sky, skyerr,
