dddd45416dedca469d8dfbb86b33e486b407b4f7
djbrout <djbrout@gmail.com>
2016-10-06 13:00:44 -0400

    testing stars

--- smp.py
+++ smp.py
@@ -1919,7 +1919,7 @@ class smp:
                     #print x_star1[0]-x_star1n[0]
                     #raw_input('stopped')
                     #sys.exit()
-                    zpt,zpterr,zpt_file = self.getzpt(x_star1+.1,y_star1+.1,tras,tdecs,starcat,mag,sky,skyerr,snparams.mjd[j],
+                    zpt,zpterr,zpt_file = self.getzpt(x_star1,y_star1,tras,tdecs,starcat,mag,sky,skyerr,snparams.mjd[j],
                                          badflagx,mag_star,im,weights,mask,maskfile,psffile,imfile,snparams,params.substamp,mjdoff,mjdslopeinteroff,j,
                                          longimfile,psf=self.psf,mjd=str(float(snparams.mjd[j])))
                     # zpt, zpterr, zpt_file = self.getzpt(x_starold, y_starold, starcat.ra[cols], starcat.dec[cols], starcat, mag, sky, skyerr,
