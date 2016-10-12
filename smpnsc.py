465d0e0eaed288ee6242de37231a1550af6bd801
djbrout <djbrout@gmail.com>
2016-10-12 16:44:47 -0400

    added smp with new starcat method

--- smpnsc.py
+++ smpnsc.py
@@ -1972,6 +1972,8 @@ class smp:
                     #raw_input('stopped')
                     #sys.exit()
                     zpttime = time.time()
+                    print x_star1.shape,tras.shape,mag_star.shape,mag.shape,sky.shape,badflagx.shape
+                    raw_input()
                     zpt,zpterr,zpt_file = self.getzpt(x_star1,y_star1,tras,tdecs,starcat,mag,sky,skyerr,snparams.mjd[j],
                                          badflagx,mag_star,im,weights,mask,maskfile,psffile,imfile,snparams,params.substamp,mjdoff,mjdslopeinteroff,j,
                                          longimfile,psf=self.psf,mjd=str(float(snparams.mjd[j])))
