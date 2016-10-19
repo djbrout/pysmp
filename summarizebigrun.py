be5dcd4442a0ad24de3ea70d2a3122432eee9504
Dillon Brout <djbrout@gmail.com>
2016-10-19 15:15:13 -0400

    sims nosn no sky

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -134,7 +134,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt):
 
     #f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(16, 12))
     gs = gridspec.GridSpec(1, 2, width_ratios=[4, 1])
     ax1 = plt.subplot(gs[0])
     ax2 = plt.subplot(gs[1])
