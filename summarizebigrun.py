d399b3597a3514b3ece0b6327bba5c84e89fdb4e
Dillon Brout <djbrout@gmail.com>
2016-10-19 14:11:05 -0400

    sims nosn no sky

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -21,7 +21,7 @@ import dilltools as dt
 
 resultsdir = '/pnfs/des/scratch/pysmp/smp_02_simnosnnoskyerr'
 isfermigrid = True
-cacheddata = True
+cacheddata = False
 
 def go(resultsdir,isfermigrid=False):
 
