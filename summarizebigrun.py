78eedbeff01bbe106d70d3b7c8a42395cf7a3f0d
Dillon Brout <djbrout@gmail.com>
2016-10-19 21:06:12 -0400

    sims nosn no sky

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -24,7 +24,7 @@ import dilltools as dt
 fakedir='/pnfs/des/scratch/pysmp/DESY1_imgList_fake/'
 resultsdir = '/pnfs/des/scratch/pysmp/smp_02_simnosnnoskyerr'
 isfermigrid = True
-cacheddata = False
+cacheddata = True
 cd = 'tmp_snse.npz'
 def go(resultsdir,isfermigrid=False):
 
@@ -291,8 +291,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag):
     ax, ay, aystd = bindata(hostmag, d, np.arange(min(hostmag), max(hostmag), .5))
     ax1.errorbar(ax, ay, aystd, markersize=20, color='green', fmt='o', label='SMP')
 
-    ax1.plot([18.5, 25.5], [0, 0])
-    ax1.set_xlim(18.5, 25.5)
+    ax1.plot([20, 28], [0, 0])
+    ax1.set_xlim(20, 28)
     ax1.set_ylim(-4., 4.)
     ax1.set_xlabel('Host Mag')
     ax1.set_ylabel('STD')
