281a9ec3695ec66b8cd3ee0322e8717058db5c28
Dillon Brout <djbrout@gmail.com>
2016-10-20 13:25:03 -0400

    sims nosn no sky

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -239,8 +239,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag):
     ax1.set_xlabel('Fake Mag')
     ax1.set_ylabel('STD')
 
-    ax, ayrms= dt.binrms(fakemag, d, np.arange(19.5, max(fakemag), .1),.5)
-    ax3.plot(ax, ayrms, color='blue',label='RMS',linewidth=3)
+    #ax, ayrms= dt.binrms(fakemag, d, np.arange(19.5, max(fakemag), .1),.5)
+    #ax3.plot(ax, ayrms, color='blue',label='RMS',linewidth=3)
 
 
     ax3.plot([0,100],[1.,1.],linestyle='--',color='black')
@@ -261,6 +261,19 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag):
     ax, ay, aystd = dt.bindata(fakemag,fresid,
                             np.arange(min(fakemag), max(fakemag), .1),window=1.)
     ax4.plot([19, 28], [0, 0],color='grey')
+
+    ax, ayrms = dt.binrms(fakemag, d, np.arange(min(hostmag), max(hostmag), .1), .5)
+    ax3.plot(ax, ayrms, color='blue', label='ALL SNe', linewidth=3)
+    ax3.plot(ax, ax * 0 + 1., linestyle='--', color='black')
+
+    ww = hostmag > 25.
+    ax, ayrms = dt.binrms(fakemag[ww], d[ww], np.arange(min(fakemag), max(fakemag), .1), .5)
+    ax3.plot(ax, ayrms, color='red', label='HostMag > 25.', linewidth=3)
+
+    ww = hostmag < 25.
+    ax, ayrms = dt.binrms(fakemag[ww], d[ww], np.arange(min(fakemag), max(fakemag), .1), .5)
+    ax3.plot(ax, ayrms, color='green', label='HostMag < 25', linewidth=3)
+    ax3.legend(fontsize='small')
 
     ax4.plot(ax, ay, linewidth=3, color='orange')
     ax4.plot(ax, ay+aystd, linewidth=2, color='orange',linestyle='--')
