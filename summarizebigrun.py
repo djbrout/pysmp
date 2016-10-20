f17ca844bf50299bb474194d1d603a97d8100092
Dillon Brout <djbrout@gmail.com>
2016-10-19 21:21:59 -0400

    sims nosn no sky

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -292,7 +292,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag):
     ax1.errorbar(ax, ay, aystd, markersize=20, color='green', fmt='o', label='SMP')
 
     ax1.plot([21.5, 27], [0, 0])
-    ax1.set_xlim(21,5, 27)
+    ax1.set_xlim(21.5, 27)
     ax1.set_ylim(-4., 4.)
     ax1.set_xlabel('Host Mag')
     ax1.set_ylabel('STD')
