597390d6d78c7f546dc49b5c299f2ba41fc5cb10
Dillon Brout <djbrout@gmail.com>
2016-10-21 12:58:31 -0400

    sims nosn no sky

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -133,6 +133,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag):
     fakezpt = np.asarray(fakezpt)
     fakeflux = 10 ** (.4 * (31. - fakemag))
     fakeflux *= 10 ** (-1 * .4 * (fitzpt - fakezpt))
+    print np.sqrt(10**(.4*(fitzpt - hostmag))/3.)
+    raw_input()
     fluxerr = np.sqrt(np.asarray(fluxerr)**2+(abs(flux)/3.) + 10**(.4*(fitzpt - hostmag))/3.)
     hostmag = np.array(hostmag)
 
