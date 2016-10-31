be57516611efe6c5fee2ed50c4e3d7fb39f2cb9d
Dillon Brout <djbrout@gmail.com>
2016-10-30 21:15:35 -0400

    adding chisq to zeropoint data

--- smpnsc.py
+++ smpnsc.py
@@ -4043,14 +4043,15 @@ class smp:
                     weight = 1./sigtot**2
                     chisqvec.append(np.sum((im - sim) ** 2 * weight * fitrad))
                     fluxvec.append(i)
-                    #print 'sigtot',sigtot,'weight',weight,'chisqvec',chisqvec[-1]
+                    print 'sigtot',sigtot,'weight',weight,'chisqvec',chisqvec[-1]
                 fluxvec = np.array(fluxvec)
                 chisqvec = np.array(chisqvec)
                 #print 'argmin guesscale',np.argmin(chisqvec)
                 guess_scale = fluxvec[np.argmin(chisqvec)]
                 guessrange = 5000
 
-            #print guess_scale
+            print guess_scale
+            raw_input()
             chisqvec = []
             fluxvec = []
             if guessrange is None:
