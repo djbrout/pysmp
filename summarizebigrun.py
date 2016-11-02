9813a811e7e5adc2e28b473d2a03ca27b4a43cf4
Dillon Brout <djbrout@gmail.com>
2016-11-02 14:58:49 -0400

    getting ready to do big faint fit

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -828,7 +828,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,title=
     ax, ayrms = dt.binrms(catmag, dmam, np.arange(16., max(catmag), .1), .5)
     ax3.plot(ax, ayrms, color='orange', label='ZPT Scatter and ChisMin Err', linewidth=3,alpha=.95)
     ax3.plot(ax, ax * 0 + 1., linestyle='--', color='black')
-    ax3.legend(fontsize='x-small')
+    ax3.legend(fontsize='x-small',loc=3)
     # ww = hostmag > 25.
     # ax, ayrms = dt.binrms(catmag[ww], d[ww], np.arange(19.5, max(fakemag), .1), .5)
     # ax3.plot(ax, ayrms, color='red', label='HostMag > 25.', linewidth=3)
