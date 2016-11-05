5cc376b65e3758d91ca0fabc318a5292304c09d6
Dillon Brout <djbrout@gmail.com>
2016-11-05 13:06:47 -0400

    TESTING HOLTZMANN

--- graballsnfiles.py
+++ graballsnfiles.py
@@ -15,16 +15,17 @@ for filename in files:
     #print filename
     if not '-X1' in filename:
         continue
+    if '.idx' in filename: continue
     #if not 'SN-E1' in filename: continue
-    if os.path.isfile("/pnfs/des/persistent/smp/v2/"+filename):
-        if os.stat("/pnfs/des/persistent/smp/v2/"+filename).st_size > 0.:
+    if os.path.isfile("/pnfs/des/persistent/smp/v4/"+filename):
+        if os.stat("/pnfs/des/persistent/smp/v4/"+filename).st_size > 0.:
             print filename,'already exists'
             continue
     print 'globus copying',filename
     out = os.popen( 'globus-url-copy -nodcau -cred /tmp/x509up_u48121 '
                 '-ss "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=dtn03-garchive.nersc.gov"'
-                ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v2/' +
-                filename+' gsiftp://fndca1.fnal.gov:2811/des/persistent/smp/v2/'+filename).read()
+                ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v4/' +
+                filename+' gsiftp://fndca1.fnal.gov:2811/des/persistent/smp/v4/'+filename).read()
     print out
     print filename
 
