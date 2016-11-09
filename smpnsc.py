fe0b96567cb97cc5cb9dcff6fb3abcb4fdc8c62e
Dillon Brout <djbrout@gmail.com>
2016-11-09 16:09:42 -0500

    added ps1 automation

--- smpnsc.py
+++ smpnsc.py
@@ -713,8 +713,8 @@ class smp:
             if not band == filt:
                 continue
             skysig=np.nan
-            if cntrs > 1:
-               continue
+            #if cntrs > 1:
+            #   continue
             #if snparams.mjd[j] != 56636.:
             #    if snparams.mjd[j] < 57000.:
             #        continue
@@ -1237,8 +1237,8 @@ class smp:
             #if round(snparams.mjd[j]) != 56559:
             #    continue
             #raw_input('passed')
-            if cccc > 5:
-                continue
+            #if cccc > 5:
+            #    continue
             if filt != 'all' and band not in filt:
                 # print('filter %s not in filter list %s for image file %s'%(band,filt,imfile))
                 # print 'filter %s,%s not in filter list for image file %s'%(band,filt,imfile)
