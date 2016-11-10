9957b8da47a0fd13f8e555b2ec4036d0de2ac40c
Dillon Brout <djbrout@gmail.com>
2016-11-10 09:58:03 -0500

    added ps1 automation

--- pytask.py
+++ pytask.py
@@ -21,7 +21,7 @@ i = -1
 while i < 23:
     cntr += 1
     #print os.listdir(os.path.join(smpdir,'lightcurves'))
-    print lightcurves[cntr][:9]+lightcurves[cntr][11:].split('.')[0]+'.smp'
+    print lightcurves[cntr][:11]+lightcurves[cntr][9:].split('.')[0]+'.smp'
     raw_input()
     if lightcurves[cntr][:9]+lightcurves[cntr][11:].split('.')[0]+'.smp' in os.listdir(os.path.join(smpdir,'lightcurves')):
         print lightcurves[cntr].split('.')[0]+'.smp', 'already exists'
