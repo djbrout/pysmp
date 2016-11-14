2168f96f833dce518a6d00ce2d284c1fe18f957c
Dillon Brout <djbrout@gmail.com>
2016-11-13 19:22:29 -0500

    big DES run

--- graballsnfiles.py
+++ graballsnfiles.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-f = open('sntarfilesv4.txt','r').read()
+f = open('sntarfilesv5.txt','r').read()
 files = f.split()
 
 #sys.exit()
@@ -17,8 +17,8 @@ for filename in files:
     #    continue
     if '.idx' in filename: continue
     cntr += 1
-    if '-S1' in filename: continue
-    if '-S2' in filename: continue
+    #if '-S1' in filename: continue
+    #if '-S2' in filename: continue
 
     #if cntr < 20: continue
     #if not 'SN-E1' in filename: continue
@@ -29,8 +29,8 @@ for filename in files:
     print 'globus copying',filename
     out = os.popen( 'globus-url-copy -nodcau -cred /tmp/x509up_u48121 '
                 '-ss "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=dtn03-garchive.nersc.gov"'
-                ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v4/' +
-                filename+' gsiftp://fndca1.fnal.gov:2811/des/persistent/smp/v4/'+filename).read()
+                ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v5/' +
+                filename+' gsiftp://fndca1.fnal.gov:2811/des/persistent/smp/v5/'+filename).read()
     print out
     print filename
 
