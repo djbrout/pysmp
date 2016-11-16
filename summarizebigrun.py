22902e764b8f9fa761683e49628929902108978a
Dillon Brout <djbrout@gmail.com>
2016-11-16 14:09:16 -0500

    big DES run

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -145,7 +145,7 @@ def grabdata(tmpwriter,resultsdir,cd):
     #deep = 0
     tot = len(smpfiles)
     cntr = 0
-    for f in smpfiles[3:]:
+    for f in smpfiles[2:]:
         cntr += 1
         print cntr, 'of',tot
         deep = 0
