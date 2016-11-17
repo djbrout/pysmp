6c0ba477a3b913ead0e995dfc7839222d547d383
Dillon Brout <djbrout@gmail.com>
2016-11-17 13:17:52 -0500

    adding real and fake keywords

--- graballsnfiles.py
+++ graballsnfiles.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-f = open('sntarfilesv4.txt','r').read()
+f = open('sntarfilesv2.txt','r').read()
 files = f.split()
 
 #sys.exit()
@@ -18,7 +18,7 @@ for filename in files:
     #filename = filename.replace('v4','v5')
     if '.idx' in filename: continue
     cntr += 1
-    #if not '-S1' in filename: continue
+    if not '-E1' in filename: continue
     #if not '-S2' in filename: continue
 
     if cntr < 30: continue
@@ -30,8 +30,8 @@ for filename in files:
     print 'globus copying',filename
     out = os.popen( 'globus-url-copy -nodcau -cred /tmp/x509up_u48121 '
                 '-ss "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN=dtn03-garchive.nersc.gov"'
-                ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v4/' +
-                filename+' gsiftp://fndca1.fnal.gov:2811/des/persistent/smp/v4/'+filename).read()
+                ' gsiftp://garchive.nersc.gov:2811/home/projects/dessn/diffim/FinalPhoto/v2/' +
+                filename+' gsiftp://fndca1.fnal.gov:2811/des/persistent/smp/v2/'+filename).read()
     print out
     print filename
 
