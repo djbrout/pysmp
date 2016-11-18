d632fee08c171bd7b49ac8617f48eaa0516c1fd8
Dillon Brout <djbrout@gmail.com>
2016-11-17 22:49:18 -0500

    fxing wrong image name issue Y1 - P1

--- smpnsc.py
+++ smpnsc.py
@@ -4639,7 +4639,8 @@ class smp:
         #     mag_cat *= -1.
         prevra = 0
         for x,y,m,s,se,mc,ra,dec,i in zip(xstar,ystar,mags,sky,skyerr,mag_cat,ras,decs,range(len(xstar))):
-            print x,y,m
+            print x,y,mc
+            print self.snparams.nxpix, self.snparams.nypix
             if round(prevra,5) == round(ra,5):
                 #print 'same star, so skipping'
                 continue
