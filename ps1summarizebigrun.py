09dd5185e1b079facd38e9bca86cfa96ba958c65
Dillon Brout <djbrout@gmail.com>
2016-11-21 17:31:36 -0500

    testing shift psf in fourier for PS1

--- ps1summarizebigrun.py
+++ ps1summarizebigrun.py
@@ -120,7 +120,7 @@ def grabstardata(imagedir,outfile):
                         md, std = iterstat.iterstat(float(zp) - cm[ww] - 2.5*np.log10(fs[ww]),
                                                      startMedian=True, sigmaclip=3, iter=10)
                         print 'worked now std',std
-                        bigdata['rmsaddin'].extend(zptdata['flux_starh']*0. + std)
+                        bigdata['rmsaddin'].extend(zptdata['fitflux']*0. + std)
                         #print 'read in ',fname
                         zptfiles.append(fname)
                         cntr += 1
