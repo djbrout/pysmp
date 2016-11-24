8fae68d999f83b4f6ef39657f418585ace59bff9
Dillon Brout <djbrout@gmail.com>
2016-11-24 12:21:29 -0500

    testing mapshiftpsf old method for PS1

--- mcmc.py
+++ mcmc.py
@@ -1359,7 +1359,7 @@ class metropolis_hastings():
                             thispsf = chkpsf_fast.fit( self.fullims[epoch],self.impsfs[epoch],self.hpsfs[epoch],
                                                      xpos = self.x[epoch] + self.x_pix_offset,
                                                      ypos = self.y[epoch] + self.y_pix_offset)
-                            thispsfcenter =  [self.x[epoch] + self.x_pix_offset, self.y[epoch] + self.y_pix_offset]
+                            thispsfcenter =  [self.y[epoch] + self.y_pix_offset,self.x[epoch] + self.x_pix_offset]
                             print epoch, self.x[epoch] + self.x_pix_offset, self.psfcenter[epoch][0]
                     # if thispsfcenter[0] != self.psfcenter[epoch][0] or thispsfcenter[1] != self.psfcenter[epoch][1]:
                     #     newpsf = thispsf
