65d5a9c5b668d2d826f6ad121f5e10b9353b2ada
Dillon Brout <djbrout@gmail.com>
2016-11-27 14:44:40 -0500

    testing scalefactor inside ps1 psf shift

--- smpnsc.py
+++ smpnsc.py
@@ -1735,7 +1735,7 @@ class smp:
             xsn = xsn[0]
             ysn = ysn[0]
             if self.snparams.survey == 'PS1':
-                xsn += 2
+                xsn += 1
                 ysn += 2
             #print xsn,ysn
             #raw_input('stopppppppppp')
