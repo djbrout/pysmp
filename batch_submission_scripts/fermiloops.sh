8fa01e944c4927698d45cfbfe5353f40e60a2724
Dillon Brout <djbrout@gmail.com>
2016-12-02 10:32:18 -0500

    added gains dependent on ccdnum

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -6,6 +6,6 @@ source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup jobsub_client
 
 
-jobsub_submit -G des --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=600s -M --verbose --OS=SL6\
+jobsub_submit -G des --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=18h -M --verbose --OS=SL6\
  --email-to=djbrout@gmail.com -N 1100 file://runfermirun.csh
 echo done
