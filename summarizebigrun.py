67d4cc17d279da9de775fe477de8bd65132cf7d2
Dillon Brout <djbrout@gmail.com>
2016-12-03 11:04:38 -0500

    added gains dependent on ccdnum

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -170,9 +170,9 @@ def grabdata(tmpwriter,resultsdir,cd):
             #print data['ID_OBS']
             #raw_input()
             print len(data['FLUX']),len(data['FLUXERR']),len(data['FAKEMAG']),len(data['ZPT']),(data['FAKEZPT'])
-            data2 = dt.readcol('./working/lightcurves/' + f.split('/')[-1])
-            rms = np.mean(data2['RMSADDIN'][data2['RMSADDIN'] > 0.0])
-            bigdata['rmsaddin'].extend(data['CHI2'] * 0. + rms)
+            #data2 = dt.readcol('./working/lightcurves/' + f.split('/')[-1])
+            #rms = np.mean(data2['RMSADDIN'][data2['RMSADDIN'] > 0.0])
+            #bigdata['rmsaddin'].extend(data['CHI2'] * 0. + rms)
 
             bigdata['Flux'].extend(data['FLUX'])
             bigdata['Fluxerr'].extend(data['FLUXERR'])
