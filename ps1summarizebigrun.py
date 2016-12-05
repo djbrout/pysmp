d40e875ed0f5a6092e196a690bd29492f4c623f6
Dillon Brout <djbrout@gmail.com>
2016-12-04 21:45:41 -0500

    added gains dependent on ccdnum

--- ps1summarizebigrun.py
+++ ps1summarizebigrun.py
@@ -371,7 +371,7 @@ def plotsigma(flux,fluxerr,dflux,dfluxerr,deltapmjd,chisq,outdir):
 
     #plt.scatter(deltapmjd[wwn],flux[wwn])
     plt.errorbar(deltapmjd,flux,yerr=fluxerr,fmt='o',label='SMP',alpha=.4)
-    plt.errorbar(ddeltapmjd,dflux,yerr=dfluxerr,fmt='o',label='Diffimg',alpha=.4)
+    plt.errorbar(ddeltapmjd,dflux,yerr=dfluxerr,fmt='o',label='Diffimg',alpha=.4 )
     plt.xlabel('Days after Peak MJD')
     plt.ylabel('Flux')
     plt.legend()
