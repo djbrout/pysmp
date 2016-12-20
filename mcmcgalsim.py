03be5b330523a90d4cfae00102bd6ccdbfc49c30
Dillon Brout <djbrout@gmail.com>
2016-12-20 11:53:26 -0500

    mapkernel

--- mcmcgalsim.py
+++ mcmcgalsim.py
@@ -906,6 +906,8 @@ class metropolis_hastings():
         #save_fits_image(self.data[0,:,:],'./out/MDJ'+str(self.mjd)+'data.fits')
         #return self.modelvec_params, self.modelvec_uncertainty, self.galmodel_params, self.galmodel_uncertainty, self.modelvec_nphistory, self.galmodel_nphistory, self.sims,np.asarray(self.xhistory),np.asarray(self.yhistory),self.accepted_history,self.model_substamp,self.chisq,self.ra_nphistory,self.dec_nphistory # size: self.history[num_iter,len(self.model_params)]
 
+        stamps = [datastamps, simstamps, galmodelstamps, weightstamps, psfstamps, chisqstamps]
+        chsqs = self.csv / len(self.mask[self.mask > 0.].ravel())
         return self.modelvec_params, self.modelvec_uncertainty, self.galmodel_params, self.galmodel_uncertainty, self.modelvec_nphistory, self.galmodel_nphistory, self.sims,np.asarray(self.xhistory),np.asarray(self.yhistory),self.accepted_history, self.model_substamp, self.chisq, self.chisq, stamps, chsqs  # size: self.history[num_iter,len(self.model_params)]
 
     def get_params_analytical_weighted( self ):
