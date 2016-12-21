397ee22e3431e01c5b58da8f159cf35bfeb5cc67
Dillon Brout <djbrout@gmail.com>
2016-12-20 19:25:14 -0500

    gsparams reduced precision

--- mcmcgalsim.py
+++ mcmcgalsim.py
@@ -265,8 +265,8 @@ class metropolis_hastings():
 
         self.model_pixel_scale_galsim = self.model_pixel_scale * galsim.arcsec
         self.model_wcs = galsim.PixelScale(self.model_pixel_scale_galsim/galsim.arcsec)
-        self.big_fft_params = galsim.GSParams(maximum_fft_size=2024000,folding_threshold=1.e-2,maxk_threshold=1.e-2)
-        self.psfparams = galsim.GSParams(maximum_fft_size=2024000,kvalue_accuracy=1.e-3,folding_threshold=1.e-2,maxk_threshold=1.e-2)
+        self.big_fft_params = galsim.GSParams(maximum_fft_size=2024000,folding_threshold=1.e-1,maxk_threshold=1.e-1)
+        self.psfparams = galsim.GSParams(maximum_fft_size=2024000,kvalue_accuracy=1.e-3,folding_threshold=1.e-1,maxk_threshold=1.e-1)
 
         self.kicked_snraoff = copy(self.snraoff)
         self.kicked_sndecoff = copy(self.sndecoff)
