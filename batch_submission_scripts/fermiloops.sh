f23fa7590dcabd84c0cedcd0a636c8270f218daf
Dillon Brout <djbrout@gmail.com>
2016-12-22 01:57:42 -0500

    big run with galsim coming

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -6,6 +6,6 @@ source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup jobsub_client
 
 
-jobsub_submit -G des --memory=1500MB --disk=100GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=10h -M --verbose --OS=SL6\
+jobsub_submit -G des --memory=1500MB --disk=100GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=24h -M --verbose --OS=SL6\
  --email-to=djbrout@gmail.com -N 40 file://runfermirun.csh
 echo done
