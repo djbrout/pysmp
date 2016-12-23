0a150e2c15caf5b3d3d1264eec2bfb087d9f5de4
Dillon Brout <djbrout@gmail.com>
2016-12-22 23:46:43 -0500

    big run with galsim coming

--- smpnsc.py
+++ smpnsc.py
@@ -2572,8 +2572,8 @@ class smp:
                                     if self.snparams.survey == 'PS1':
                                         noise_stamp[noise_stamp > 0.] = 1
                                         noise_stamp[noise_stamp <= 0.001] = 0
-                                        print skyerrsn,skysig
-                                        raw_input()
+                                        #print skyerrsn,skysig
+                                        #raw_input()
                                         smp_noise[i,:,:] = noise_stamp*1/(skyerrsn*scalefactor)**2 * mask
                                     else:
                                         noise_stamp[noise_stamp > 0.] = 1
