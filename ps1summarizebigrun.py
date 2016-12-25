9624cad566afce17346c3cd35e390dae03e08f75
Dillon Brout <djbrout@gmail.com>
2016-12-25 17:48:08 -0500

    not doing globalstar. now centroiding for every star!

--- ps1summarizebigrun.py
+++ ps1summarizebigrun.py
@@ -370,8 +370,8 @@ def plotsigma(flux,fluxerr,dflux,dfluxerr,deltapmjd,chisq,outdir):
     plt.clf()
     deltapmjd = deltapmjd[ww]
 
-    wwn = (deltapmjd > 480.) | (deltapmjd < -120.)
-    dwwn = (ddeltapmjd > 480.) | (ddeltapmjd < -120.)
+    wwn = (deltapmjd > 480.) | (deltapmjd < -100.)
+    dwwn = (ddeltapmjd > 480.) | (ddeltapmjd < -100.)
 
 
     #plt.scatter(deltapmjd[wwn],flux[wwn])
