f5c7650252a2b2e2a4fce8faa06c1e5f6ddc8fbc
Dillon Brout <djbrout@gmail.com>
2016-12-27 12:33:20 -0500

    not doing globalstar. now centroiding for every star!

--- ps1summarizebigrun.py
+++ ps1summarizebigrun.py
@@ -514,7 +514,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
     plt.clf()
     np.savez('simnosn.npz',flux=flux,fakeflux=ff,fluxerr=np.sqrt(fluxerr**2 + abs(flux)/3.8))
 
-    d = (flux - fakeflux) / ((fluxerr**2 - abs(flux)/3.8 + frms**2)**.5*1.08)
+    d = (flux - fakeflux) / ((fluxerr**2 - abs(flux)/3.8 + frms**2)**.5*1.0)
 
     ww = (flux != 0.) #& (deep == 0)
 
