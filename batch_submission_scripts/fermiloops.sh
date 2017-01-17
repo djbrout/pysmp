e00b5b0e22d2334d6e6ffa5190a08d1fa995bea3
Dillon Brout <djbrout@gmail.com>
2017-01-17 12:37:31 -0500

    keeping track of centroided ra and dec in zpt files

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -6,6 +6,6 @@ source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup jobsub_client
 
 
-jobsub_submit -G des --memory=2000MB --disk=59GB --cpu=4 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=48h -M --verbose --OS=SL6\
+jobsub_submit -G des --memory=2000MB --disk=59GB --cpu=4 --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=8h -M --verbose --OS=SL6\
  --email-to=djbrout@gmail.com -N 1000 file://runfermirun.csh
 echo done
