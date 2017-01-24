6caefdf807450f0997c3c2eb8b6d07c884d3b9aa
Dillon Brout <djbrout@gmail.com>
2017-01-24 16:01:39 -0500

    keeping track of centroided ra and dec in zpt files

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -72,7 +72,7 @@ def grabstardata(imagedir,outfile):
     zptfiles = []
     cntr = 0
     for dirName, subdirList, fileList in os.walk(imagedir):
-        if cntr > 10.: continue
+        if cntr > 10.: break
         #print('Found directory: %s' % dirName)
         for fname in fileList:
 
