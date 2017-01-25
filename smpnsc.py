47a55e429a62e5c80a2eb95d967cfae6c5df467b
Dillon Brout <djbrout@gmail.com>
2017-01-25 12:57:38 -0500

    keeping track of centroided ra and dec in zpt files

--- smpnsc.py
+++ smpnsc.py
@@ -2524,8 +2524,8 @@ class smp:
                         sexsky,sexrms = runsextractor.getsky_and_skyerr(imfile,im,xlow-100+stampsize,xhi+100-stampsize,
                                                                         ylow-100+stampsize,yhi+100-stampsize,snparams.survey)
 
-                        print sexsky,sexrms
-                        raw_input('sextractor')
+                        #print sexsky,sexrms
+                        #raw_input('sextractor')
                         bkgrnd = pf.getdata(imfile + '.background')
                         print bkgrnd.shape
                         sexsky = np.mean(bkgrnd[ylow:yhi,xlow:xhi].ravel()) * scalefactor
@@ -2534,7 +2534,7 @@ class smp:
                         #sexsky *= scalefactor
                         #sexrms *= scalefactor
                         print sexsky, sexrms
-                        #raw_input('youyo')
+                        raw_input('youyo')
 
                     skyvals = im[ylow:yhi,xlow:xhi].ravel()
                     #print im.shape
