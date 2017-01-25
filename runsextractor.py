bd0eea0178b11a0c2fd1902baee6f9abc18df0b5
Dillon Brout <djbrout@gmail.com>
2017-01-25 15:39:11 -0500

    sextractor rms and sky in fits

--- runsextractor.py
+++ runsextractor.py
@@ -43,7 +43,8 @@ def getsky_and_skyerr(imagefilename,im,xlow,xhi,ylow,yhi,survey='DES'):
             workdir='sewpy_logs/'
             , sexpath=sexpath
             , loglevel="CRITICAL"
-            , config={"checkimage_type":"BACKGROUND,BACKGROUND_RMS","checkimage_name":imagefilename+'.background, '+imagefilename+'.background_rms'}
+            , config={"checkimage_type":"BACKGROUND,BACKGROUND_RMS","checkimage_name":imagefilename+'.background, '+imagefilename+'.background_rms'
+                      ,"background_mesh_size":"256"}
 
         )
     out = sew(imagefilename)
