3a4841b988fcc1aa1223a6fe179cef301d39dcf1
Dillon Brout <djbrout@gmail.com>
2017-01-29 01:57:45 -0500

    sextractor rms and sky in fits

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -196,6 +196,7 @@ def grabdata(tmpwriter,resultsdir,cd,filter = 'r',oldformat=False):
     for f in smpfiles[:]:
         cntr += 1
         if cntr > 10000: continue
+        if cntr == 34: continue
         print cntr, 'of',tot
         deep = 0
         os.system('cp '+f+' test.npz')
