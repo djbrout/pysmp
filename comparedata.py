c92fb3af0b6baba3cc05a23b38ed6f85b3abd0f9
Dillon Brout <djbrout@gmail.com>
2017-02-07 13:19:12 -0500

    trying to fix skybuff issue

--- comparedata.py
+++ comparedata.py
@@ -94,7 +94,7 @@ for i,r in enumerate(residstamp):
 
         ax = plt.subplot(330+cntr%9)
         print np.array(r[0,:,:]).shape
-        print max(np.array(r[0,:,:])),print min(np.array(r[0,:,:]))
+        print max(np.array(r[0,:,:])), min(np.array(r[0,:,:]))
 
 
         ax.imshow(np.array(r[0]), cmap='gray', interpolation='nearest')
