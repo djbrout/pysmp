a68fb5a9fb83fae54b8d436aa83c14f78eba853a
Dillon Brout <djbrout@gmail.com>
2017-02-07 14:41:39 -0500

    trying to fix skybuff issue

--- comparedata.py
+++ comparedata.py
@@ -54,7 +54,7 @@ for x in np.arange(substamp):
 for i,f in enumerate(commonfiles):
     v6dat = np.load(v6dir+f)
     v4dat = np.load(v4dir + f)
-    print i
+    print v6dat.keys()
 
     try:
         for j,m in enumerate(v6dat['mjd']):
