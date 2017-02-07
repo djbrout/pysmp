df3fba61e83778fc8d254ba072a9a24868133d30
Dillon Brout <djbrout@gmail.com>
2017-02-07 08:06:22 -0500

    trying to fix skybuff issue

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -7,5 +7,5 @@
 
 
 jobsub_submit -G des --memory=1500MB --disk=59GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=15h -M --verbose --OS=SL6\
- --email-to=djbrout@gmail.com -N 12 file://runfermirun.csh
+ --email-to=djbrout@gmail.com -N 13 file://runfermirun.csh
 echo done
