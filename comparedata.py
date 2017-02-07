3aeeb51b303baba862e074da3e4ede7533dbafa3
Dillon Brout <djbrout@gmail.com>
2017-02-07 14:57:19 -0500

    trying to fix skybuff issue

--- comparedata.py
+++ comparedata.py
@@ -128,8 +128,8 @@ for i,r in enumerate(residstamp):
         plt.subplot(132)
         plt.title('V6')
 
-        mn = min(np.array(bigv6stamps[i]-bigv6sky[i]).ravel().extend(np.array(bigv4stamps[i]-bigv4sky[i])))
-        mx = max(np.array(bigv6stamps[i]-bigv6sky[i]).ravel().extend(np.array(bigv4stamps[i]-bigv4sky[i])))
+        mn = min((bigv6stamps[i]-bigv6sky[i]).ravel().extend((bigv4stamps[i]-bigv4sky[i])))
+        mx = max((bigv6stamps[i]-bigv6sky[i]).ravel().extend((bigv4stamps[i]-bigv4sky[i])))
 
         plt.imshow(np.array(bigv6stamps[i]-bigv6sky[i]), cmap='gray', interpolation='nearest',vmin=mn,vmax=mx)
         plt.colorbar()
