3becb37052703a79d3f1c0df1aa631b826feeb0c
Dillon Brout <djbrout@gmail.com>
2017-02-08 16:36:10 -0500

    trying to fix skybuff issue

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -6,6 +6,6 @@
 #setup jobsub_client
 
 
-jobsub_submit -G des --memory=1500MB --disk=59GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=15h -M --verbose --OS=SL6\
+jobsub_submit -G des --memory=1500MB --force=xrootd --disk=59GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --expected-lifetime=15h -M --verbose --OS=SL6\
  -N 250 file://runfermirun.csh
 echo done
