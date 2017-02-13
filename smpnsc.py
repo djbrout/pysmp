690d55b73c21f00d9a11e629d9d07ec748aa1de1
Dillon Brout <djbrout@gmail.com>
2017-02-13 15:43:18 -0500

    trying to fix skybuff issue

--- smpnsc.py
+++ smpnsc.py
@@ -2173,7 +2173,7 @@ class smp:
             #print self.gain
             #raw_input('gain')
             if self.snparams.survey == 'DES':
-                im += 0.
+                im += 2000.
             mjdoff = 0.
             mjdslopeinteroff = 0.
             gogo = True
