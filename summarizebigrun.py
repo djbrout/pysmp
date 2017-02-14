a4881580b257db67df037a8fd34b7e002134db18
Dillon Brout <djbrout@gmail.com>
2017-02-13 23:25:21 -0500

    trying to fix skybuff issue

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -1446,7 +1446,7 @@ if __name__ == "__main__":
     fakedir = '/pnfs/des/scratch/pysmp/DESY1_imgList_fake/'
     resultsdir = '/pnfs/des/scratch/pysmp/smp_04_modelerrors'
     resultsdir = '/pnfs/des/scratch/pysmp/smp_02_simnosnnoskyerr'
-    resultsdir = '/pnfs/des/scratch/pysmp/smp_v625'
+    resultsdir = '/pnfs/des/scratch/pysmp/smp_v626'
     #resultsdir= './working/'
     #resultsdir= '/export/scratch0/ps1sn1/data/v10.0/GPC1v3/eventsv1/smpworkspace/PS_TEST1/'
     #resultsdir = './workingsimnosn'
