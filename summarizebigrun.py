3fc5dac9d543c8c5b62095b0d28ad5ccf16148fd
Dillon Brout <djbrout@gmail.com>
2017-02-15 21:16:20 -0500

    trying to fix skybuff issue

--- summarizebigrun.py
+++ summarizebigrun.py
@@ -473,7 +473,21 @@ def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,sky,dpmjd,chisq,imfi
     plt.ylabel('Flux Difference ')
     plt.savefig(outdir + '/fluxdiff.png')
 
+    ww = (flux != 0.) & (fakemag != 0)  # (fakemag < 28.5) & (flux != 0.)
+    plt.clf()
+    fig = plt.figure(figsize=(15, 10))
+    plt.scatter(fakeflux[ww], (flux[ww] - fakeflux[ww])/fakeflux[ww], alpha=.5)
+    ax, ay, aystd = bindata(fakeflux[ww], (flux[ww] - fakeflux[ww])/fakeflux[ww],
+                            np.arange(-100,5000, 200))
+    plt.errorbar(ax, ay, aystd, markersize=10, color='green', fmt='o', label='SMP')
 
+    plt.axhline(0)
+    plt.xlim(0, 5000)
+    # plt.ylim(-.1,.1)
+    plt.ylim(-.1, .1)
+    plt.xlabel('Fake Flux')
+    plt.ylabel('Flux Difference ')
+    plt.savefig(outdir + '/fluxdiff.png')
 
 
     skyresid = 2000. - sky / 10 ** (.4 * (31 - fitzpt))
