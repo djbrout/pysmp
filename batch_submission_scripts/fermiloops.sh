e8fd3f57c6404a05e8eecca168078f4461df481d
Dillon Brout <djbrout@gmail.com>
2017-02-19 18:25:36 -0500

    trying to fix skybuff issue

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -8,5 +8,5 @@ rm /tmp/*x509up_u`id -u`*
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup jobsub_client
 jobsub_submit -G des --memory=1500MB --maxConcurrent=50 --disk=59GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --generate-email-summary --email-to=bdrizzle@yahoo.com --mail_never --expected-lifetime=15h -M --verbose --OS=SL6\
- -N 1000 file://runfermirun.csh
+ -N 1800 file://runfermirun.csh
 echo done
