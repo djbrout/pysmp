14433b93901f4e209e6c10003ea360c20e93f386
Dillon Brout <djbrout@gmail.com>
2017-02-21 09:41:38 -0500

    trying to fix skybuff issue

--- fermiman.py
+++ fermiman.py
@@ -11,7 +11,7 @@ def getfakefiles(ccd,field,num):
 
     for fk in allfieldfakes:
 
-        print fk
+        print "awk 'FNR > 150 { nextfile }; /g_"+ccd+".LIST/ { print FILENAME }' "+ fk +" | uniq > data/"+field.lower()+"lightcurves.txt"
         out = os.popen("awk 'FNR > 150 { nextfile }; /g_"+ccd+".LIST/ { print FILENAME }' "+ fk +
                  " | uniq > data/"+field.lower()+"lightcurves.txt").read()
 
