9231ecf04b6b834e8609a5db5e3ab9fe63ae93c5
Dillon Brout <djbrout@gmail.com>
2017-02-21 09:28:07 -0500

    trying to fix skybuff issue

--- batch_submission_scripts/fermiloops.sh
+++ batch_submission_scripts/fermiloops.sh
@@ -7,6 +7,6 @@
 rm /tmp/*x509up_u`id -u`*
 source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
 setup jobsub_client
-jobsub_submit -G des --memory=1500MB --maxConcurrent=50 --disk=59GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --generate-email-summary --email-to=bdrizzle@yahoo.com --mail_never --expected-lifetime=15h -M --verbose --OS=SL6\
- -N 1800 file://runfermirun.csh
+jobsub_submit -G des --memory=1500MB --disk=59GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --generate-email-summary --email-to=bdrizzle@yahoo.com --mail_never --expected-lifetime=15h -M --verbose --OS=SL6\
+ -N 100 file://runfermirun.csh
 echo done
---
+++ fermiman.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+
+def getfakefiles(ccd,field,num):
+    allfieldfakes = open('data/'+field.lower()+'lightcurves.txt').readlines()
+    goodfakes = []
+    for fk in allfieldfakes:
+
+        if len(goodfakes) == num:
+            break
+
+        stopreading = False
+        for l in open(fk).readlines():
+            if 'g_'+ccd+'.LIST' in l:
+                print fk
+                goodfakes.append(fk)
+                stopreading = True
+            if stopreading:
+                break
+
+
+    return goodfakes
+
+def setup(ccd,field,num):
+
+    fakelist = getfakefiles(ccd,field,num)
+
+    ccdnums = []
+    for l in fakelist:
+        ll = open(l).readlines()[80,120]
+        for li in ll:
+            imfile = li.split(' ')[13]
+            ccdnums.append(int(imfile.split('/')[-1].split('_')[-1][:2]))
+
+
+    ccdnums = np.unique(np.array(ccdnums,dtype='int'))
+
+    print ccdnums
+
+
+
+
+
+
+if __name__ == "main":
+    import sys,getopt
+    try:
+        args = sys.argv[1:]
+        opt, arg = getopt.getopt(
+            args, "c:f:n",
+            longopts=["ccd=","field=","num="])
+    except getopt.GetoptError as err:
+        print str(err)
+        print "Error : incorrect option or missing argument."
+        print __doc__
+        sys.exit(1)
+
+
+    ccd, field, num = None, None, 5
+
+
+
+    for o, a in opt:
+        if o in ["-c", "--ccd"]:
+            ccd = a
+        elif o in ["-f","--field"]:
+            field = a
+
+    setup(ccd,field)
