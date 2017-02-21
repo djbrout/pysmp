0343ffd2d96d478ec7e95891a9c2671306f99aa1
Dillon Brout <djbrout@gmail.com>
2017-02-21 09:45:27 -0500

    trying to fix skybuff issue

--- greplightcurves.sh
+++ greplightcurves.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-awk 'FNR > 50 { nextfile }; /SN-S2/ { print FILENAME }' /pnfs/des/scratch/pysmp/imglistfake/des_fake_*.dat | uniq > data/s2lightcurves.txt
+awk 'FNR > 50 { nextfile }; /SN-S1/ { print FILENAME }' /pnfs/des/scratch/pysmp/imglistfake/des_fake_*.dat | uniq > data/s1lightcurves.txt
