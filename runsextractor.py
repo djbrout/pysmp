6a2cf8133bb24b81eff5d9925688822f965b9e08
Dillon Brout <djbrout@gmail.com>
2017-02-26 19:20:09 -0500

    trying to fix skybuff issue

--- runsextractor.py
+++ runsextractor.py
@@ -63,6 +63,8 @@ def getsky_and_skyerr(imagefilename,im,xlow,xhi,ylow,yhi,survey='DES',index=''):
 
     try:
         os.remove(newfilename)
+        os.remove('/global/cscratch1/sd/dbrout/sewpy_logs/'+index+'_'+imagefilename.split('/')[-1]+'.cat.txt')
+        os.remove('/global/cscratch1/sd/dbrout/sewpy_logs/'+index+'_'+imagefilename.split('/')[-1]+'.log.txt')
         os.remove('/global/cscratch1/sd/dbrout/sewpy_logs/'+index+'_'+imagefilename.split('/')[-1]+'.background')
         os.remove('/global/cscratch1/sd/dbrout/sewpy_logs/'+index+'_'+imagefilename.split('/')[-1]+'.background_rms')
     except:
