571ffa1ed58a904351a62f4a7d8a25d42ea3b471
Dillon Brout <djbrout@gmail.com>
2017-02-28 12:44:14 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -49,7 +49,7 @@ def go(fakedir,resultsdir,cacheddata,cd,filter,isfermigrid=False):
     #raw_input()
     plotpercentageresid(data['Flux'],data['Fluxerr'],data['FakeMag'],data['FitZPT'],data['diffzpt'], data['diffimflux'],
                         data['sky'],data['DPMJD'],data['Chisq'],data['imfiles'],data['ra'],data['dec'],
-                        data['image_stamp'],resultsdir+'/Summary/'+filter+'/',data['fakefiles'],data['HostMag'],filter)
+                        data['image_stamp'],resultsdir+'/Summary/'+filter+'/',data['fakefiles'],data['HostMag'],filter,data['FakeZPT'])
     plotsigmaresid(data['Flux'],data['Fluxerr'],data['FakeMag'], data['FitZPT'], data['diffzpt'],data['HostMag'],
                    data['Chisq'],data['rmsaddin'],data['field'],resultsdir+'/Summary/'+filter+'/')#resultsdir)
     #starmag = stardata['starzpt'] - 2.5*np.log10(stardata['starflux'])
@@ -365,7 +365,7 @@ def grabdata(tmpwriter,resultsdir,cd,filter = 'g',oldformat=False):
     return bigdata
 
 
-def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,diffimflux,sky,dpmjd,chisq,imfiles,ra,dec,imstamp,outdir,fakefiles,hostmag,filter):
+def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,diffimflux,sky,dpmjd,chisq,imfiles,ra,dec,imstamp,outdir,fakefiles,hostmag,filter,oldfakezpt):
     flux = np.asarray(flux)
     fakemag = np.asarray(fakemag)
     sky = np.asarray(sky)
@@ -381,6 +381,7 @@ def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,diffimflux,sky,dpmjd
     fluxerr = np.asarray(fluxerr)
     fakezpt = np.asarray(fakezpt)
     diffimflux = np.array(diffimflux)
+    oldfakezpt = np.array(oldfakezpt)
     # print fakezpt
     #
     # print fakemag.shape
@@ -392,7 +393,7 @@ def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,diffimflux,sky,dpmjd
     fakezpt = np.asarray(fakezpt)
 
     fakeflux = 10**(.4*(31. - fakemag))
-    diffimflux *= 10**(-1*.4*(31. - fakezpt))
+    diffimflux *= 10**(.4*(31. - oldfakezpt))
 
     # for fm,ff,fl in zip(fakemag,fakeflux,flux):
     #
