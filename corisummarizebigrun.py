dfe8f2d9c95b51072ec447114d7208f3280d8fb5
Dillon Brout <djbrout@gmail.com>
2017-02-28 11:53:49 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -792,7 +792,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
     plt.clf()
     np.savez('simnosn.npz',flux=flux,fakeflux=ff,fluxerr=np.sqrt(fluxerr**2 + abs(flux)/3.8))
 
-    d = (flux - fakeflux) / ((fluxerr**2  )**.5)
+    d = (flux - fakeflux) / ((fluxerr**2 + abs(flux) )**.5)
 
     ww = (flux != 0.) #& (deep == 0)
 
