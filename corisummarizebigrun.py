bb198c5d02c18abf5b628f0dc45e962f8afc04fb
Dillon Brout <djbrout@gmail.com>
2017-03-01 15:52:26 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -590,32 +590,32 @@ def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,diffimflux,sky,dpmjd
     # plt.ylabel('Flux Difference ')
     # plt.savefig(outdir + '/efluxdiff.png')
 
-    plt.clf()
-    fig = plt.figure(figsize=(15, 10))
-    plt.hist(flux[ww]/fluxerr[ww], bins=np.arange(-6.2, 6, .4),normed=True,label='RMS Fakemag = 99: ' + str(round(rms99, 3)))
-    # ax, ay, aystd = bindata(fakeflux[ww], (flux[ww] - fakeflux[ww]),
-    #                        np.arange(-100, 1000, 200))
-    # plt.errorbar(ax, ay, aystd, markersize=10, color='green', fmt='o', label='SMP')
-
-    # plt.axhline(0)
-    plt.xlim(-5, 5)
-
-    import matplotlib.mlab as mlab
-    import math
-    mean = 0
-    variance = 1
-    sigma = math.sqrt(variance)
-    x = np.arange(-5, 5, .1)
-    plt.plot(x, mlab.normpdf(x, mean, sigma), color='black', label='Gaussian Normal')
-    plt.legend()
-    # plt.ylim(-.1,.1)
-    # plt.ylim(-600, 600)
-    plt.xlabel('flux/fluxerr')
-    plt.ylabel('Count')
-    plt.title(filter+' band')
-
-
-    plt.savefig(outdir + '/efluxdiffstd.png')
+    # plt.clf()
+    # fig = plt.figure(figsize=(15, 10))
+    # plt.hist(flux[ww]/fluxerr[ww], bins=np.arange(-6.2, 6, .4),normed=True,label='RMS Fakemag = 99: ' + str(round(rms99, 3)))
+    # # ax, ay, aystd = bindata(fakeflux[ww], (flux[ww] - fakeflux[ww]),
+    # #                        np.arange(-100, 1000, 200))
+    # # plt.errorbar(ax, ay, aystd, markersize=10, color='green', fmt='o', label='SMP')
+    #
+    # # plt.axhline(0)
+    # plt.xlim(-5, 5)
+    #
+    # import matplotlib.mlab as mlab
+    # import math
+    # mean = 0
+    # variance = 1
+    # sigma = math.sqrt(variance)
+    # x = np.arange(-5, 5, .1)
+    # plt.plot(x, mlab.normpdf(x, mean, sigma), color='black', label='Gaussian Normal')
+    # plt.legend()
+    # # plt.ylim(-.1,.1)
+    # # plt.ylim(-600, 600)
+    # plt.xlabel('flux/fluxerr')
+    # plt.ylabel('Count')
+    # plt.title(filter+' band')
+    #
+    #
+    # plt.savefig(outdir + '/efluxdiffstd.png')
 
     ww = (flux != 0.) & (fakemag != 0)  # (fakemag < 28.5) & (flux != 0.)
     plt.clf()
@@ -931,31 +931,31 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
 
     ww = (flux != 0) & (np.array(fakemag,dtype='float') > 90.)
 
-    # fig = plt.figure(figsize=(15, 10))
-    # plt.hist(flux[ww] / fluxerr[ww], bins=np.arange(-6.2, 6, .4), normed=True,
-    #          label='RMS Fakemag = 99: ' + str(round(rms99, 3)))
-    # # ax, ay, aystd = bindata(fakeflux[ww], (flux[ww] - fakeflux[ww]),
-    # #                        np.arange(-100, 1000, 200))
-    # # plt.errorbar(ax, ay, aystd, markersize=10, color='green', fmt='o', label='SMP')
-    #
-    # # plt.axhline(0)
-    # plt.xlim(-5, 5)
-    #
-    # import matplotlib.mlab as mlab
-    # import math
-    # mean = 0
-    # variance = 1
-    # sigma = math.sqrt(variance)
-    # x = np.arange(-5, 5, .1)
-    # plt.plot(x, mlab.normpdf(x, mean, sigma), color='black', label='Gaussian Normal')
-    # plt.legend()
-    # # plt.ylim(-.1,.1)
-    # # plt.ylim(-600, 600)
-    # plt.xlabel('flux/fluxerr')
-    # plt.ylabel('Count')
-    # plt.title(filter + ' band')
-    #
-    # plt.savefig(outdir + '/efluxdiffstd.png')
+    fig = plt.figure(figsize=(15, 10))
+    plt.hist(flux[ww] / fluxerr[ww], bins=np.arange(-6.2, 6, .4), normed=True,
+             label='RMS Fakemag = 99: ' + str(round(rms99, 3)))
+    # ax, ay, aystd = bindata(fakeflux[ww], (flux[ww] - fakeflux[ww]),
+    #                        np.arange(-100, 1000, 200))
+    # plt.errorbar(ax, ay, aystd, markersize=10, color='green', fmt='o', label='SMP')
+
+    # plt.axhline(0)
+    plt.xlim(-5, 5)
+
+    import matplotlib.mlab as mlab
+    import math
+    mean = 0
+    variance = 1
+    sigma = math.sqrt(variance)
+    x = np.arange(-5, 5, .1)
+    plt.plot(x, mlab.normpdf(x, mean, sigma), color='black', label='Gaussian Normal')
+    plt.legend()
+    # plt.ylim(-.1,.1)
+    # plt.ylim(-600, 600)
+    plt.xlabel('flux/fluxerr')
+    plt.ylabel('Count')
+    plt.title(filter + ' band')
+
+    plt.savefig(outdir + '/efluxdiffstd.png')
 
 
     nullfmt = NullFormatter()  # no labels
