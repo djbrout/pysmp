9e73bdac4294e6577b93152bbba368fc6332c683
Dillon Brout <djbrout@gmail.com>
2017-03-01 11:16:13 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -218,8 +218,8 @@ def grabdata(tmpwriter,resultsdir,cd,filter = 'g',oldformat=False):
         os.system('cp '+f+' test.npz')
         data = dt.readcol('test.npz')
         tra = data['RA']
-        #print data.keys()
-        #raw_input()
+        print data.keys()
+        raw_input()
         #print tra[0]
         dra = np.zeros(len(dofakera))+tra[0]
         cra = np.isclose(dra,dofakera,atol=1.e-3)
