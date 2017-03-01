9e21d54177efe2fc1ca1ee4ef8f40a3fb942799e
Dillon Brout <djbrout@gmail.com>
2017-03-01 12:25:13 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -270,8 +270,8 @@ def grabdata(tmpwriter,resultsdir,cd,filter = 'g',oldformat=False):
             #raw_input()
             # ifm = (dofakemag2 == fm)
             #print exn, fm
-            print fm,dofakemag2[expn],exn
-            raw_input()
+            #print fm,dofakemag2[expn],exn
+            #raw_input()
             if not len(dofakemag2[www]) > 0:
                 newfakemag.append(99.)
                 #print fm, 99
