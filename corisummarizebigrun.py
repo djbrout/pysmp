3a16fb63d948d6994fbd155f4624c203e3d9ee61
Dillon Brout <djbrout@gmail.com>
2017-03-01 15:45:16 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -590,32 +590,32 @@ def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,diffimflux,sky,dpmjd
     # plt.ylabel('Flux Difference ')
     # plt.savefig(outdir + '/efluxdiff.png')
 
-    plt.clf()
-    fig = plt.figure(figsize=(15, 10))
-    plt.hist(flux[ww]/fluxerr[ww], bins=np.arange(-6.2, 6, .4),normed=True,label='RMS Fakemag = 99: ' + str(round(rms99, 3)))
-    # ax, ay, aystd = bindata(fakeflux[ww], (flux[ww] - fakeflux[ww]),
-    #                        np.arange(-100, 1000, 200))
-    # plt.errorbar(ax, ay, aystd, markersize=10, color='green', fmt='o', label='SMP')
-
-    # plt.axhline(0)
-    plt.xlim(-5, 5)
-
-    import matplotlib.mlab as mlab
-    import math
-    mean = 0
-    variance = 1
-    sigma = math.sqrt(variance)
-    x = np.arange(-5, 5, .1)
-    plt.plot(x, mlab.normpdf(x, mean, sigma), color='black', label='Gaussian Normal')
-    plt.legend()
-    # plt.ylim(-.1,.1)
-    # plt.ylim(-600, 600)
-    plt.xlabel('flux/fluxerr')
-    plt.ylabel('Count')
-    plt.title(filter+' band')
-
-
-    plt.savefig(outdir + '/efluxdiffstd.png')
+    # plt.clf()
+    # fig = plt.figure(figsize=(15, 10))
+    # plt.hist(flux[ww]/fluxerr[ww], bins=np.arange(-6.2, 6, .4),normed=True,label='RMS Fakemag = 99: ' + str(round(rms99, 3)))
+    # # ax, ay, aystd = bindata(fakeflux[ww], (flux[ww] - fakeflux[ww]),
+    # #                        np.arange(-100, 1000, 200))
+    # # plt.errorbar(ax, ay, aystd, markersize=10, color='green', fmt='o', label='SMP')
+    #
+    # # plt.axhline(0)
+    # plt.xlim(-5, 5)
+    #
+    # import matplotlib.mlab as mlab
+    # import math
+    # mean = 0
+    # variance = 1
+    # sigma = math.sqrt(variance)
+    # x = np.arange(-5, 5, .1)
+    # plt.plot(x, mlab.normpdf(x, mean, sigma), color='black', label='Gaussian Normal')
+    # plt.legend()
+    # # plt.ylim(-.1,.1)
+    # # plt.ylim(-600, 600)
+    # plt.xlabel('flux/fluxerr')
+    # plt.ylabel('Count')
+    # plt.title(filter+' band')
+    #
+    #
+    # plt.savefig(outdir + '/efluxdiffstd.png')
 
     ww = (flux != 0.) & (fakemag != 0)  # (fakemag < 28.5) & (flux != 0.)
     plt.clf()
@@ -915,7 +915,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
 
     plt.clf()
 
-    dc = d[abs(d) < 3]
+    dc = d[abs(d) < 3.]
 
     dc99 = d[np.array(fakemag,dtype='float') > 90.]
     rms99 = np.sqrt(np.nanmean(np.square(dc99[abs(dc99) < 3.])))
@@ -985,8 +985,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
     ax3.xaxis.set_major_formatter(nullfmt)
     ax5.yaxis.set_major_formatter(nullfmt)
 
-    outliers3 = float(len(d[d>3.]))/float(len(d))
-    outliers5 = float(len(d[d>5.]))/float(len(d))
+    outliers3 = float(len(d[abs(d)>3.]))/float(len(d))
+    outliers5 = float(len(d[abs(d)>5.]))/float(len(d))
 
     ax2.hist(d, bins=np.arange(-10, 10, .25), normed=True,label='RMS Fakemag = 99: ' + str(round(rms99, 3))+
                                                                 '\nRMS Fakemag < 99: '+ str(round(rmsr, 3))+'\n3sig Outlier'+
