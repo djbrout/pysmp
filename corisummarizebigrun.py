cde169c0e1c89bd627c4f7ce6089a7c4f1ed08a4
Dillon Brout <djbrout@gmail.com>
2017-03-04 11:16:42 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -994,8 +994,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
     outliers3 = float(len(d[abs(d)>3.]))/float(len(d))
     outliers5 = float(len(d[abs(d)>5.]))/float(len(d))
 
-    ax2.hist(d, bins=np.arange(-10, 10, .25), normed=True,label='RMS Fakemag = 99: ' + str(round(rms99, 3))+
-                                                                '\nRMS Fakemag < 99: '+ str(round(rmsr, 3))+'\n3sig Outlier'#+
+    ax2.hist(d, bins=np.arange(-10, 10, .25), normed=True#,label='RMS Fakemag = 99: ' + str(round(rms99, 3))+
+                                                                #'\nRMS Fakemag < 99: '+ str(round(rmsr, 3))+'\n3sig Outlier'#+
                                                                 #' Fraction: '+str(round(outliers3,3))+'\n5sig Outlier'+
                                                                 #' Fraction: '+str(round(outliers5,3))
                                                                 ,orientation='horizontal')
