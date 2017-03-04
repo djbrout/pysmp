559b8ed8c862675fa3a683ae599879afe5a466cc
Dillon Brout <djbrout@gmail.com>
2017-03-04 09:33:26 -0500

    trying to fix skybuff issue

--- smpnsc.py
+++ smpnsc.py
@@ -2588,7 +2588,8 @@ class smp:
                     mean,st,vals = sigma_clip.meanclip(im[ylow:yhi,xlow:xhi],clipsig = 4, maxiter = 8)
 
                     plt.clf()
-                    plt.hist(im[ylow:yhi,xlow:xhi].ravel(),bins=15)
+                    plt.hist(im[ylow:yhi,xlow:xhi].ravel(),bins=np.arange(5405,5600,10))
+
                     plt.savefig('testimvar.png')
 
                     skysig=1.48*np.median(abs(vals-np.median(vals)))
