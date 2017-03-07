3e85c802f93f5d08a7948b2a91fd526cae87648d
Dillon Brout <djbrout@gmail.com>
2017-03-07 12:23:15 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1872,7 +1872,7 @@ if __name__ == "__main__":
     fakedir = '/project/projectdirs/des/djbrout/pysmp/imglist/all/'
     #resultsdir = '/pnfs/des/scratch/pysmp/smp_04_modelerrors'
     #resultsdir = '/pnfs/des/scratch/pysmp/smp_02_simnosnnoskyerr'
-    resultsdir = '/project/projectdirs/des/djbrout/v71shift/'
+    resultsdir = '/project/projectdirs/des/djbrout/v72/'
     #resultsdir= './working/'
     #resultsdir= '/export/scratch0/ps1sn1/data/v10.0/GPC1v3/eventsv1/smpworkspace/PS_TEST1/'
     #resultsdir = './workingsimnosn'
