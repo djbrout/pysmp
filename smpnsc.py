d43d9ea4e551d7db74997cbbaebced08fcc620c5
Dillon Brout <djbrout@gmail.com>
2017-03-06 20:59:13 -0500

    trying to fix skybuff issue

--- smpnsc.py
+++ smpnsc.py
@@ -3747,7 +3747,7 @@ class smp:
                     , fitzpt = smp_dict['zpt']
                     , fakezpt = smp_dict['fakezpt']
                     , datafilenames = smp_dict['image_filename']
-                    , shiftgalstd = .001
+                    , shiftgalstd = .0001
 
                     )
             modelveco = copy(modelvec)
