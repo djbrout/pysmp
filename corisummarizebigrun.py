36b26a2797e3b61bf1ff8b5f4bb8df5027509c06
Dillon Brout <djbrout@gmail.com>
2017-03-07 16:34:05 -0500

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -553,7 +553,7 @@ def plotpercentageresid(flux,fluxerr,fakemag,fitzpt,fakezpt,diffimflux,sky,skyer
     plt.savefig(outdir + '/fluxerrvsskyerr.png')
 
     plt.clf()
-    plt.hist(skyerr, bins=np.arange(0,200,20))
+    plt.hist(skyerr, bins=np.arange(0,200,5.),normed=True)
     plt.xlabel('SMP Skyerr')
     plt.xlim(0, 200)
     plt.savefig(outdir + '/skyerrhist.png')
