5aa42308d9079521a74ebafdb1fbdac721c3e284
Dillon Brout <djbrout@gmail.com>
2017-03-08 11:16:39 -0500

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -3098,10 +3098,14 @@ class smp:
                         print ind
                         ww = smp_starind[k,:] == ind
                         if len(ww) > 0:
-                            print 'worked'
-                            nightlyoffra.append(float(meanstarras[ind] - smp_starra[k,ww]))
-                            nightlyoffdec.append(float(meanstardecs[ind] - smp_stardec[k, ww]))
-                            nightlydist.append(((smp_dict['snra'][k] - smp_starra[k,ww])**2+(smp_dict['sndec'][k] - smp_stardec[k,ww])**2)**.5)
+                            try:
+                                float(meanstarras[ind] - smp_starra[k, ww])
+                                nightlyoffra.append(float(meanstarras[ind] - smp_starra[k,ww]))
+                                nightlyoffdec.append(float(meanstardecs[ind] - smp_stardec[k, ww]))
+                                nightlydist.append(((smp_dict['snra'][k] - smp_starra[k,ww])**2+(smp_dict['sndec'][k] - smp_stardec[k,ww])**2)**.5)
+                                print 'worked'
+                            except:
+                                pass
 
                 nightlyoffra = np.array(nightlyoffra)
                 nightlyoffdec = np.array(nightlyoffdec)
