1269de299be8f4c01371fc57635778104eea6914
Dillon Brout <djbrout@gmail.com>
2017-03-11 15:32:13 -0500

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -3145,8 +3145,8 @@ class smp:
                     xsno = xsno[0]
                     ysno = ysno[0]
 
-                    smp_dict['yoff'][k] = xsno-xsn + smp_dict['snx'][k] - round(smp_dict['snx'][k])
-                    smp_dict['xoff'][k] = ysno-ysn + smp_dict['sny'][k] - round(smp_dict['sny'][k])
+                    smp_dict['yoff'][k] = xsno-xsn #+ smp_dict['snx'][k] - round(smp_dict['snx'][k])
+                    smp_dict['xoff'][k] = ysno-ysn #+ smp_dict['sny'][k] - round(smp_dict['sny'][k])
 
                     # x_psf,y_psf = cntrd.cntrd(smp_psf[k,:,:],15,15,2.)
                     # print x_psf,y_psf,-smp_dict['snx'][k]+round(smp_dict['snx'][k]),\
