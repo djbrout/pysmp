a463ed10b2ba5dd6b8e6511136fc63691f8bb3b3
Dillon Brout <djbrout@gmail.com>
2017-03-12 09:04:55 -0400

    trying to fix skybuff issue

--- batch_submission_scripts/loop.py
+++ batch_submission_scripts/loop.py
@@ -4,42 +4,43 @@ import numpy as np
 import time
 
 allindexes = range(0,49)
-filt = 'g'
+filts = ['g','r','i','z']
 #np.random.shuffle(allindexes)
 
 for i in allindexes:
-    print i
-    script = '/global/cscratch1/sd/dbrout/logs/sm_' + str(i) + '.sh'
-    f = open(script, 'w')
-    f.write(
-        '#!/bin/bash -l\n' +
-        '#SBATCH --partition=shared\n' +
-        '#SBATCH -n 1\n' +
-        '#SBATCH -c 1\n'+
-        '#SBATCH -C haswell\n'+
-        '#SBATCH -A dessn\n' +
-        '#SBATCH --time=02:49:00\n' +
-        '#SBATCH --output=/global/cscratch1/sd/dbrout/speclogs/' + str(i) + '_'+filt+'spec.log\n' +
-        '#SBATCH --error=/global/cscratch1/sd/dbrout/speclogs/' + str(i) + '_'+filt+'spec.log\n' +
-        '#SBATCH --job-name=spec'+filt+'_' + str(i) + '\n' +
-        '#SBATCH --mail-type=NONE\n' +
-        #'#SBATCH --qos=premium\n'+
-        '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
-        '#SBATCH --gres=craynetwork:1\n' +
-        '\n' +
-        'cd /project/projectdirs/des/djbrout/pysmp/\n' +
-        'source setup_scripts/setupcori2.sh\n'+
-        #'source /scratch3/scratchdirs/masao/setup_DiffImg.sh\n'
-        'echo "RUNNING NOW"\n'+
-        #'python test.py\n'
-        #'cd /global/u1/d/dbrout/SEaR/\n' +
-        #'echo "--start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+'" \n'+
-        #'python mpp.py --start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+' \n'
-        #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
-        'python smpshift.py --index=' + str(i) + ' -f '+filt+' \n' +
-        '\n'
-    )
-    f.close()
-    output = Popen(["sbatch", script], stdout=PIPE).communicate()
-    print output[0]
-    #time.sleep(1)
+    for filt in filts:
+        print i
+        script = '/global/cscratch1/sd/dbrout/logs/sm_' + str(i) + '.sh'
+        f = open(script, 'w')
+        f.write(
+            '#!/bin/bash -l\n' +
+            '#SBATCH --partition=shared\n' +
+            '#SBATCH -n 1\n' +
+            '#SBATCH -c 1\n'+
+            '#SBATCH -C haswell\n'+
+            '#SBATCH -A dessn\n' +
+            '#SBATCH --time=02:49:00\n' +
+            '#SBATCH --output=/global/cscratch1/sd/dbrout/speclogs/' + str(i) + '_'+filt+'spec.log\n' +
+            '#SBATCH --error=/global/cscratch1/sd/dbrout/speclogs/' + str(i) + '_'+filt+'spec.log\n' +
+            '#SBATCH --job-name=spec'+filt+'_' + str(i) + '\n' +
+            '#SBATCH --mail-type=NONE\n' +
+            #'#SBATCH --qos=premium\n'+
+            '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
+            '#SBATCH --gres=craynetwork:1\n' +
+            '\n' +
+            'cd /project/projectdirs/des/djbrout/pysmp/\n' +
+            'source setup_scripts/setupcori2.sh\n'+
+            #'source /scratch3/scratchdirs/masao/setup_DiffImg.sh\n'
+            'echo "RUNNING NOW"\n'+
+            #'python test.py\n'
+            #'cd /global/u1/d/dbrout/SEaR/\n' +
+            #'echo "--start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+'" \n'+
+            #'python mpp.py --start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+' \n'
+            #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
+            'python smpshift.py --index=' + str(i) + ' -f '+filt+' \n' +
+            '\n'
+        )
+        f.close()
+        output = Popen(["sbatch", script], stdout=PIPE).communicate()
+        print output[0]
+        #time.sleep(1)
