eaf61cc7f6be814d36fb79b459e642a6924659df
Dillon Brout <djbrout@gmail.com>
2017-03-12 22:05:40 -0400

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -3789,8 +3789,8 @@ class smp:
             aaa = mcmc3.metropolis_hastings(
                     galmodel = galmodel
                     , modelvec = modelvec
-                    , galstd = galstd
-                    , modelstd = modelstd
+                    , galstd = galstd/2.
+                    , modelstd = modelstd*2.
                     , data = smp_im
                     , psfs = smp_psf
                     , weights = smp_noise
