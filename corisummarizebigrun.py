932accd160b1094b804b4e96adb71eed2e547831
Dillon Brout <djbrout@gmail.com>
2017-03-14 00:21:05 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1010,8 +1010,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
     # ax1 = plt.subplot(gs[0])
     # ax2 = plt.subplot(gs[1])
 
-    ww = (flux != 0) & (np.array(fakemag,dtype='float') > 90.) & (fluxerr >0.) & (np.isfinite(flux)) & \
-         (np.isfinite(fluxerr)) &(~np.isnan(flux)) &(~np.isnan(fluxerr)) & (chisqarr > .5) & (chisqarr < 1.4)
+    ww = (flux != 0.) & (np.array(fakemag,dtype='float') > 90.) & (fluxerr >0.) & (np.isfinite(flux)) & \
+         (np.isfinite(fluxerr)) &(~np.isnan(flux)) &(~np.isnan(fluxerr)) & (chisqarr > .2) & (chisqarr < 2.)
 
     #ww = (flux != 0) & (fakeflux < 1.)
     #print rms99
