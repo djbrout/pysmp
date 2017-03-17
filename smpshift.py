289ddf8ca2bdd5c3954843735d466f3ee846e2f6
Dillon Brout <djbrout@gmail.com>
2017-03-17 09:15:14 -0400

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -1363,7 +1363,7 @@ class smp:
             #if round(snparams.mjd[j],2) != 56030.33:
             #    continue
             #raw_input('passed')
-            if cccc > 5000:
+            if cccc > 15:
                 continue
             if filt != 'all' and band not in filt:
                 # print('filter %s not in filter list %s for image file %s'%(band,filt,imfile))
