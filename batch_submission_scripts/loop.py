19be203f453d65eb5bcb477f1c59b22b56fb956b
Dillon Brout <djbrout@gmail.com>
2017-03-18 13:32:27 -0400

    trying to fix skybuff issue

--- batch_submission_scripts/loop.py
+++ batch_submission_scripts/loop.py
@@ -3,7 +3,7 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(29,30)
+allindexes = range(0,200)
 #filts = ['g','r','i','z']
 filts = ['g']
 #np.random.shuffle(allindexes)
@@ -20,10 +20,10 @@ for i in allindexes:
             '#SBATCH -c 1\n'+
             '#SBATCH -C haswell\n'+
             '#SBATCH -A dessn\n' +
-            '#SBATCH --time=29:49:00\n' +
-            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'spec.log\n' +
-            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'spec.log\n' +
-            '#SBATCH --job-name=spec'+filt+'_' + str(i) + '\n' +
+            '#SBATCH --time=3:49:00\n' +
+            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sim.log\n' +
+            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sim.log\n' +
+            '#SBATCH --job-name=sim'+filt+'_' + str(i) + '\n' +
             '#SBATCH --mail-type=NONE\n' +
             #'#SBATCH --qos=premium\n'+
             '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
@@ -38,7 +38,7 @@ for i in allindexes:
             #'echo "--start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+'" \n'+
             #'python mpp.py --start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+' \n'
             #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
-            'python smpshift.py --index=' + str(i) + ' -f '+filt+' --nozpt \n' +
+            'python smpshift.py --index=' + str(i) + ' -f '+filt+' \n' +
             '\n'
         )
         f.close()
