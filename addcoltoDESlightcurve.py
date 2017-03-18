799e8f8ecbdb51a8e270e90eff68238c30ca66a7
Dillon Brout <djbrout@gmail.com>
2017-03-18 13:19:08 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -122,6 +122,7 @@ if __name__ == "__main__":
             addtolightcurve(lcfile,savelcfile,sndata['MJD'],sndata['FLUX'],sndata['FLUXERR'],
                             sndata['ZPT'], sndata['RMSADDIN'],
                             sndata['CHI2'],sndata['SKY'],sndata['SKYERR'],filt=filt,saveinplace=inplace)
+            print 'SAVED SUCCESSFULLY',filt,savelcfile,
             raw_input()
 
 
