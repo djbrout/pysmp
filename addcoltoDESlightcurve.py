16ae2bb01d6f708986b3d337aa8bddf4f7f777fe
Dillon Brout <djbrout@gmail.com>
2017-03-18 17:37:31 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -147,8 +147,8 @@ if __name__ == "__main__":
             #except:
             #    print 'SMP RESULTS DO NOT EXIST FOR ', smpfile
             #raw_input()
-    print 'cd '+resultsdir+'\n tar -zcf '+savelcdir.split('/')[-1]+'.tar.gz -C '+savelcdir.split('/')[-1]+'/'
-    os.popen('cd '+resultsdir+'\n tar -zcf '+savelcdir.split('/')[-1]+'.tar.gz -C '+savelcdir.split('/')[-1]+'/')
+    print 'cd '+resultsdir+'\n tar -zcf '+savelcdir.split('/')[-1]+'.tar.gz '+savelcdir.split('/')[-1]+'/'
+    os.popen('cd '+resultsdir+'\n tar -zcf '+savelcdir.split('/')[-1]+'.tar.gz '+savelcdir.split('/')[-1]+'/')
 
 
 #addtolightcurve('testlc.dat','./testdats/','testcol',,[888,777,000,111],[8,8,8,8],[31.,31.,31.,31.])
