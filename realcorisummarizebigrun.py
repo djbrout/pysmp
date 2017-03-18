1dec303043944ba6bc3d9c99cc6ab338fd59532d
Dillon Brout <djbrout@gmail.com>
2017-03-18 11:00:26 -0400

    trying to fix skybuff issue

--- realcorisummarizebigrun.py
+++ realcorisummarizebigrun.py
@@ -1428,9 +1428,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
         ax3.xaxis.set_major_formatter(nullfmt)
         ax5.yaxis.set_major_formatter(nullfmt)
 
-        ax2.hist(d, bins=np.arange(-10, 10, .25), normed=True, label='RMS Fakemag = 99: ' + str(round(rms99, 3)) +
-                                                                     '\nRMS Fakemag < 99: ' + str(round(rmsr, 3))
-                 , orientation='horizontal')
+        ax2.hist(d, bins=np.arange(-10, 10, .25), normed=True, orientation='horizontal')
         # label='RMS: ' + str(round(rms, 3)) + '\nChiSq (3sig cut) ' + str(round(chisq, 3)) + '\nMedian ' + str(
         #   round(np.median(d), 3)) + ' +- ' + str(round(np.std(d), 3)),
 
