2aa3f417d66752bfe35b8449e3b9f4a74134c796
Dillon Brout <djbrout@gmail.com>
2017-03-18 12:24:15 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -2,21 +2,22 @@
 import numpy as np
 import os
 
-def addtolightcurve(lightcurvefile,saveloc,column_name,filt,mjd,flux,fluxerr,fakemag):
+def addtolightcurve(lightcurvefile,saveloc,column_name,mjd,flux,fluxerr,fakemag,filt=None):
     if not os.path.exists(saveloc):
         os.makedirs(saveloc)
     savefile = open(saveloc+'/'+lightcurvefile.split('/')[-1],'w')
     origfile = open(lightcurvefile,'r')
     lines = origfile.readlines()
     mjd = np.array(mjd)
-    filt = np.array(filt,dtype='str')
+    if not filt is None:
+        filt = np.array(filt,dtype='str')
     flux = np.array(flux)
     fluxerr = np.array(fluxerr)
     fakemag = np.array(fakemag)
     #zp = np.array(zp)
     for line in lines:
         if line.split(' ')[0] == 'VARNAMES:':
-            line = line.strip()+' FLUX_'+column_name.upper()+' FLUXERR_'+column_name.upper()+' FAKE_TRUEMAG_'+column_name.upper()+'\n'
+            line = line.strip()+' SMPFLUX_'+column_name.upper()+' SMPFLUXERR_'+column_name.upper()+' SMPZPT_'+column_name.upper()+'\n'
         elif line.split(' ')[0] == 'OBS:':
             id = int(line.split()[1])
             tmjd = float(line.split()[3])
