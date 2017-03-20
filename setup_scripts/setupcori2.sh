01d785a2357d79fe1d3d48215af5394f0a57306a
Dillon Brout <djbrout@gmail.com>
2017-03-20 14:48:11 -0400

    trying to fix skybuff issue

--- setup_scripts/setupcori2.sh
+++ setup_scripts/setupcori2.sh
@@ -2,5 +2,3 @@ module load python
 source /project/projectdirs/cmb/modules/hpcports_NERSC.sh
 hpcports gnu
 module load astromatic-hpcp
-source /global/project/projectdirs/dessn/diffim/setup.sh
-diffimg_scratch
