5e8b5ec8d56ca84d685d5cafe2793c49b1396271
Dillon Brout <djbrout@gmail.com>
2017-03-27 11:12:53 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -232,12 +232,12 @@ def grabdata(tmpwriter,resultsdir,cd,filter = 'g',oldformat=False):
         #raw_input()
         #print tra[0]
         dra = np.zeros(len(dofakera))+tra[0]
-        cra = np.isclose(dra,dofakera,atol=1.e-3)
+        cra = np.isclose(dra,dofakera,atol=1.e-2)
         tdec = data['DEC']
         ddec = np.zeros(len(dofakedec))+tdec[0]
-        cdec = np.isclose(ddec,dofakedec,atol=1.e-3)
+        cdec = np.isclose(ddec,dofakedec,atol=1.e-2)
 
-        print dofakera[:10]
+        print dofakera[cra & cdec]
         raw_input()
 
         if not oldformat:
