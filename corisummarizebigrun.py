5c0808b22300bdc110976ed64f8dba4da81dcee8
Dillon Brout <djbrout@gmail.com>
2017-03-27 11:54:49 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -294,8 +294,8 @@ def grabdata(tmpwriter,resultsdir,cd,filter = 'g',oldformat=False):
                 # print dofakemag2[]
                 #www = (expnum == float(exn)) & (np.isclose(float(fm), dofakemag2, atol=1.e-3))
                 www = expn & cdec & cra
-                print dofakeid[www]
-                raw_input()
+                #print dofakeid[www]
+                #raw_input()
                 # ifm = (dofakemag2 == fm)
                 #print exn, fm
                 #print fm,dofakemag2[expn],exn
