fa002948a76930c4a4b2168976d96489f34e3589
Dillon Brout <djbrout@gmail.com>
2017-04-01 13:52:36 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1866,7 +1866,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     cntr = 0
     for sme,sm,ind,r,d,cm,f,fe in zip(starmagerr,starmag,indices,ras,decs,catmag,flux,fluxerr):
         cntr+=1
-        if cntr > 1000: continue
+        if cntr > 10000: continue
         #print starmag[np.isclose(ras,r,rtol=1.e-5) & np.isclose(decs,d,rtol=1.e-5) & (catmag == cm)]
         #print starmag[indices == ind]
         #raw_input()
@@ -1890,7 +1890,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     pltvec = []
     for sme, sm, ind, r, d, cm in zip(starmagerr, starmag, indices, ras, decs, catmag):
         cntr += 1
-        if cntr > 1000: continue
+        if cntr > 10000: continue
         # print starmag[np.isclose(ras,r,rtol=1.e-5) & np.isclose(decs,d,rtol=1.e-5) & (catmag == cm)]
         # print starmag[indices == ind]
         # raw_input()
@@ -1915,7 +1915,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     pltvec = []
     for sme, sm, ind, r, d, cm,cs in zip(starmagerr, starmag, indices, ras, decs, catmag,chisq):
         cntr += 1
-        if cntr > 1000: continue
+        if cntr > 10000: continue
         # print starmag[np.isclose(ras,r,rtol=1.e-5) & np.isclose(decs,d,rtol=1.e-5) & (catmag == cm)]
         # print starmag[indices == ind]
         # raw_input()
@@ -1928,7 +1928,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
             pltvec.append(sme - repeatability)
 
     plt.xscale('log')
-    plt.xlim(0.5, 200.)
+    plt.xlim(0.5, 500.)
     plt.ylim(-.01, .01)
     plt.ylabel('photerr - repeatability')
     plt.plot([0., 200], [0, 0], color='black')
