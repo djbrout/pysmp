0e7bb2aeaa4ef0fb25557ebfb971771fb5398039
Dillon Brout <djbrout@gmail.com>
2017-04-01 14:32:10 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2020,7 +2020,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     plt.xlim(0.0001,.01)
     plt.ylim(-.01, .01)
 
-    ax, ay, aystd = dt.bindata(np.array(pltvecx), np.array(pltvecy), np.arange(0., .1, .00005), window=.00005, dontrootn=True)
+    ax, ay, aystd = dt.bindata(np.array(pltvecx), np.array(pltvecy), np.arange(0., .1, .00005), window=.0001, dontrootn=True)
     plt.plot(ax, ay, linewidth=3, color='orange', label='SMP',alpha=.6)
     plt.plot(ax, ay + aystd, linewidth=2, color='orange', linestyle='--', label='SMP',alpha=.6)
     plt.plot(ax, ay - aystd, linewidth=2, color='orange', linestyle='--', label='SMP',alpha=.6)
