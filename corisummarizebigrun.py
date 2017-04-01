855876541aabc1d27d77710c6a07f8ee20e58652
Dillon Brout <djbrout@gmail.com>
2017-04-01 09:07:07 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1854,7 +1854,9 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
 
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlim(.0001)
+    plt.xlim(min(starmagerr),max(starmagerr))
+    plt.ylim(min(starmagerr),max(starmagerr))
+    plt.plot([min(starmagerr),max(starmagerr)],[min(starmagerr),max(starmagerr)],color='black')
     plt.savefig('repeatabilitytest.png')
 
 
