6490e18716e3624471c35634e3e72135413d8581
Dillon Brout <djbrout@gmail.com>
2017-04-01 13:02:08 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1842,6 +1842,9 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     skymagerr = -2.5*np.log10(sky) + 2.5*np.log10(sky+skyerr)
     starmagerr = (-2.5*np.log10(flux) + 2.5*np.log10(flux+fluxerr+poisson)) + rmsaddin# + (-2.5*np.log10(flux) + 2.5*np.log10(flux+poisson))#+ rmsaddin #+ (-2.5*np.log10(flux) + 2.5*np.log10(flux+poisson))**2)**.5
 
+
+    starmagerr = 1.0857*np.sqrt(skyerr**2+flux)/flux + rmsaddin
+
     plt.clf()
     # repeatability = []
     # uindices = []
