292ef1e81420f6a555b8ab56bc9a6331201a2615
Dillon Brout <djbrout@gmail.com>
2017-04-08 12:03:58 -0400

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -3545,7 +3545,7 @@ class smp:
 
         for i in range(len(smp_dict['sky'])):
             print smp_dict['mjd'][i],len(smp_noise[i][smp_noise[i]*smp_mask[i] > 0.].ravel()),
-            if len(smp_noise[i][smp_noise[i]*smp_mask[i] > 0.].ravel()) < 350.:
+            if len(smp_noise[i][smp_noise[i]*smp_mask[i] > 0.].ravel()) < 500.:
                 smp_dict['flag'][i] = 1
                 print '<--FLAGGED',
 
