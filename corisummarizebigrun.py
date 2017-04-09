c0cb99255b2f6c94bd900c642837539cbd35b06f
Dillon Brout <djbrout@gmail.com>
2017-04-09 10:15:46 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2050,7 +2050,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
         # raw_input()
         starww = starmag[np.isclose(ras, r, rtol=1.e-5) & np.isclose(decs, d, rtol=1.e-5) & (catmag == cm)]
         repeatability = np.std(starww)/np.sqrt(len(starww))
-        rep[i].append(repeatability)
+        rep[i] = repeatability
         # repeatability = np.std(starmag[indices == ind])
         if len(starww) > 4.:
             # if repeatability < .3:
