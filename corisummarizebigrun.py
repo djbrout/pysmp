ee6c4f3db7a88044a6cdfd084ccddd85ed0a3e0e
Dillon Brout <djbrout@gmail.com>
2017-04-10 09:57:57 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1846,10 +1846,10 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     #starmagerr2 = ((-2.5*np.log10(flux) + 2.5*np.log10(flux+fluxerr))**2 + rmsaddin**2 + (-2.5*np.log10(flux) + 2.5*np.log10(flux+poisson))**2 )**.5
     #starmagerr3 = ((-2.5*np.log10(sky) + 2.5*np.log10(sky+skyerr))**2 + rmsaddin[ww]**2)**.5
     skymagerr = -2.5*np.log10(sky) + 2.5*np.log10(sky+skyerr)
-    starmagerrr = (-2.5*np.log10(flux) + 2.5*np.log10(flux+fluxerr+poisson)) #+ rmsaddin# + (-2.5*np.log10(flux) + 2.5*np.log10(flux+poisson))#+ rmsaddin #+ (-2.5*np.log10(flux) + 2.5*np.log10(flux+poisson))**2)**.5
+    starmagerr = (-2.5*np.log10(flux) + 2.5*np.log10(flux+fluxerr+poisson)) + rmsaddin# + (-2.5*np.log10(flux) + 2.5*np.log10(flux+poisson))#+ rmsaddin #+ (-2.5*np.log10(flux) + 2.5*np.log10(flux+poisson))**2)**.5
 
     #starmagerrr = 1.0857*fluxerr/flux
-    starmagerr = 1.0857*np.sqrt(fluxerr**2+flux)/flux + rmsaddin
+    #starmagerr = 1.0857*np.sqrt(fluxerr**2+flux)/flux + rmsaddin
 
     plt.clf()
     # repeatability = []
