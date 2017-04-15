82890daa97ad84601ad357b55aa45831dc8fd6f8
Dillon Brout <djbrout@gmail.com>
2017-04-15 10:19:08 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -176,7 +176,7 @@ def addtolightcurve(lightcurvefile,saveloc,mjd,flux,fluxerr,zpt,zptrms,chisq,sky
                     fit_zpt_std = zptdata['fit_zpt_std']
                     flux_zpt = 31.
                     tfluxerr = fluxerr[ww][0]
-                    print tflux
+                    #print tflux
 
                 tsky = sky[ww][0] - 10000.*10**(.4*(31.-fit_zpt))
                 tskyerr = skyerr[ww][0]
