fe961454f0d8d030d74f0e42bcc71b9873472658
Dillon Brout <djbrout@gmail.com>
2017-04-19 16:05:27 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -81,7 +81,7 @@ def getparametriczpt(imagedir,outfile):
     cntr = 0
     goodbigdata = copy(bigdata)
     for dirName, subdirList, fileList in os.walk(imagedir):
-        if cntr > 5000.: break
+        if cntr > 1000.: break
         # print('Found directory: %s' % dirName)
         for fname in fileList:
             # print fname
