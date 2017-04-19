d38fa9b0b9404e39d014992b872d769f22b08247
Dillon Brout <djbrout@gmail.com>
2017-04-19 14:02:56 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -146,8 +146,8 @@ def addtolightcurve(lightcurvefile,saveloc,mjd,flux,fluxerr,zpt,zptrms,chisq,sky
             #raw_input()
             #else:
             tidobs = float(line.split()[1])
-            print tidobs,idobs
-            raw_input()
+            #print tidobs,idobs
+            #raw_input()
             if int(tidobs) in idobs:
                 ww = np.isclose(idobs,tidobs,atol=0.1)# & (filt == band)
                 #print fluxerr[ww]
