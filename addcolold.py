3f64b35122dd86860c94dff8df52504987e93597
Dillon Brout <djbrout@gmail.com>
2017-04-24 14:14:07 -0400

    trying to fix skybuff issue

--- addcolold.py
+++ addcolold.py
@@ -221,7 +221,7 @@ if __name__ == "__main__":
     lcdir = '/project/projectdirs/des/djbrout/pysmp/imglist/all/'
     resultsdir = '/project/projectdirs/des/djbrout/114sim/'
 
-    savelcdir = resultsdir + '/SMP_RAW_SIMtrue_v1_10'
+    savelcdir = resultsdir + '/SMP_RAW_SIMtrue_v1_11'
     fakes = False
 
     filts = ['g', 'r', 'i', 'z', None]
