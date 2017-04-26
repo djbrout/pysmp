f1b295f5343ae0c1ef0c16ffcba25e7c51164250
Dillon Brout <djbrout@gmail.com>
2017-04-26 15:03:18 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -265,7 +265,7 @@ if __name__ == "__main__":
     lcdir = '/project/projectdirs/des/djbrout/pysmp/imglist/all/'
     resultsdir = '/project/projectdirs/des/djbrout/114sim/'
 
-    savelcdir = resultsdir+'/SMP_SIM_v1_16'
+    savelcdir = resultsdir+'/SMP_SIM_v1_16true'
     fakes = False
     faketrueflux = False
 
