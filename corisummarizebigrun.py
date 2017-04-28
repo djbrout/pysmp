3508f59716874e1a569750d888b731f60fa1d749
Dillon Brout <djbrout@gmail.com>
2017-04-27 21:08:47 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2081,7 +2081,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     print outdir + '/' + title + 'fwhmhist.png'
     plt.clf()
 
-    maxpoints = 1000
+    maxpoints = 10000
 
     cntr = 0
     pltvecx = []
