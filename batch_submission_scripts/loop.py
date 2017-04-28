102df1f2492ab905cb4d71f619b77040f9cb6d4a
Dillon Brout <djbrout@gmail.com>
2017-04-28 00:57:16 -0400

    trying to fix skybuff issue

--- batch_submission_scripts/loop.py
+++ batch_submission_scripts/loop.py
@@ -3,7 +3,7 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(0,20)
+allindexes = range(20,100)
 filts = ['g','r','i','z']
 #filts = ['g']
 #np.random.shuffle(allindexes)
