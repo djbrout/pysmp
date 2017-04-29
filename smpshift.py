1066e5f96cb1cf1ee554db9de0c1888cca465834
Dillon Brout <djbrout@gmail.com>
2017-04-29 18:46:06 -0400

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -6084,7 +6084,7 @@ class smp:
 
                 np.savez(mag_compare_out
                          , cat_magsmp=mag_cat[goodstarcols]
-                         , fit_mag=-2.5 * np.log10(fluxcol[goodstarcols])
+                         , fit_magsmp=-2.5 * np.log10(fluxcol[goodstarcols])
                          , fit_mag_err=-2.5 * np.log10(fluxcol[goodstarcols]) + 2.5 * np.log10(fluxcol[goodstarcols] + flux_star_std[goodstarcols])
                          , flux_starh=fluxcol[goodstarcols]
                          , flux_star_std=flux_star_std[goodstarcols]
