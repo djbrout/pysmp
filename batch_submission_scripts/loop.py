6793f0be6f9c8d62ccdac83667a0083e2d99de56
Dillon Brout <djbrout@gmail.com>
2017-05-02 13:45:48 -0400

    trying to fix skybuff issue

--- batch_submission_scripts/loop.py
+++ batch_submission_scripts/loop.py
@@ -3,9 +3,9 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(20,100)
-filts = ['g','r','i','z']
-#filts = ['g']
+allindexes = range(0,100)
+#filts = ['g','r','i','z']
+filts = ['g']
 #np.random.shuffle(allindexes)
 
 for i in allindexes:
@@ -20,7 +20,7 @@ for i in allindexes:
             '#SBATCH -c 1\n'+
             '#SBATCH -C haswell\n'+
             '#SBATCH -A dessn\n' +
-            '#SBATCH --time=47:00:00\n' +
+            '#SBATCH --time=0:30:00\n' +
             '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'dsim.log\n' +
             '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'dsim.log\n' +
             '#SBATCH --job-name=sim'+filt+'_' + str(i) + '\n' +
