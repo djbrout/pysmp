93f43d82910a13c2f0468c54d3c7625c9c0bf635
Dillon Brout <djbrout@gmail.com>
2017-05-05 16:22:23 -0400

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -5014,7 +5014,7 @@ class smp:
         #         print 'star too dim...'
         if not bad:
             #return fluxvec[argm], fluxvec[argm] - fluxvec[idx][0], mchisq/ndof, sum_data_minus_sim, np.sum((im - sim) ** 2 * weight * fitrad)/ndof, bad
-            return fluxmp, fluxerrmp, 1., sum_data_minus_sim, np.sum((im - sim) ** 2 * weight * fitrad)/ndof, bad
+            return fluxmp, np.sqrt(fluxerrmp**2+fluxmp), 1., sum_data_minus_sim, np.sum((im - sim) ** 2 * weight * fitrad)/ndof, bad
         else:
             return 1,1,1,1,1,True
 
