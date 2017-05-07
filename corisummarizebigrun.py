7192830b0fc095d00f7ada3d61a46ffbcf9c5e0a
Dillon Brout <djbrout@gmail.com>
2017-05-07 10:06:11 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -200,7 +200,7 @@ def grabstardata(imagedir,outfile,tfield,filt):
     cntr = 0
     goodbigdata = copy(bigdata)
     for dirName, subdirList, fileList in os.walk(imagedir):
-        if cntr > 1000.: break
+        if cntr > 100.: break
         #print('Found directory: %s' % dirName)
         for fname in fileList:
             #print fname
