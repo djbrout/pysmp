97f10bbd398b2e74707ce199b5454f28129a322a
Dillon Brout <djbrout@gmail.com>
2017-05-07 09:46:44 -0400

    trying to fix skybuff issue

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2277,7 +2277,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
         # print starmag[indices == ind]
         # raw_input()
         starww = starmag[np.isclose(ras, r, rtol=1.e-5) & np.isclose(decs, d, rtol=1.e-5) & (catmag == cm)]
-        repeatability = np.std(starww) / np.sqrt(len(starww))
+        repeatability = np.std(starww)
         # repeatability = np.std(starmag[indices == ind])
         if len(starww) > 5.:
             # if repeatability < .3:
@@ -2318,7 +2318,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
         # print starmag[indices == ind]
         # raw_input()
         starww = starmag[np.isclose(ras, r, rtol=1.e-5) & np.isclose(decs, d, rtol=1.e-5) & (catmag == cm)]
-        repeatability = np.std(starww)/np.sqrt(len(starww))
+        repeatability = np.std(starww)
         rep[i] = repeatability*np.sqrt(len(starww))
         # repeatability = np.std(starmag[indices == ind])
         if len(starww) > 4.:
@@ -2354,7 +2354,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
         # print starmag[indices == ind]
         # raw_input()
         starww = starmag[np.isclose(ras, r, rtol=1.e-5) & np.isclose(decs, d, rtol=1.e-5) & (catmag == cm)]
-        repeatability = np.std(starww) / np.sqrt(len(starww))
+        repeatability = np.std(starww)
         # repeatability = np.std(starmag[indices == ind])
         if len(starww) > 4.:
             # if repeatability < .3:
@@ -2389,7 +2389,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
         # print starmag[indices == ind]
         # raw_input()
         starww = starmag[np.isclose(ras, r, rtol=1.e-5) & np.isclose(decs, d, rtol=1.e-5) & (catmag == cm)]
-        repeatability = np.std(starww) / np.sqrt(len(starww))
+        repeatability = np.std(starww)
         # repeatability = np.std(starmag[indices == ind])
         if len(starww) > 4.:
             # if repeatability < .3:
