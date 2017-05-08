3a692af74c0a4623bf8b7e92f55d4c1e236c5598
Dillon Brout <djbrout@gmail.com>
2017-05-08 13:41:01 -0400

    trying to fix skybuff issue

--- loopaddcol.py
+++ loopaddcol.py
@@ -25,7 +25,7 @@ for i in allindexes:
         '\n' +
         'cd /project/projectdirs/des/djbrout/pysmp/\n' +
         'source setup_scripts/setupcori2.sh\n'+
-        'python addcoltoDESlightcurve.py --index=' + str(i) + ' --faketrueflux --savelcdir=/project/projectdirs/des/djbrout/116simdeep/SMP_RAW_SIMtrue_v2 '
+        'python addcoltoDESlightcurve.py --index=' + str(i) + ' --savelcdir=/project/projectdirs/des/djbrout/116simdeep/SMP_RAW_SIM_v2 '
                                                               '--resultsdir=/project/projectdirs/des/djbrout/116simdeep/ \n' +
         '\n'
     )
