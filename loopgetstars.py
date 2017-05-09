c206eeb8db0332ad24eb62d17ca4109d09988446
Dillon Brout <djbrout@gmail.com>
2017-05-09 09:08:51 -0400

    trying to fix skybuff issue

---
+++ loopgetstars.py
@@ -0,0 +1,33 @@
+from subprocess import *
+
+filters = ['r','i','z']
+
+for i in filters:
+
+    print i
+    script = '/global/cscratch1/sd/dbrout/logs/sm_' + str(i) + '.sh'
+    f = open(script, 'w')
+    f.write(
+        '#!/bin/bash -l\n' +
+        '#SBATCH --partition=shared\n' +
+        '#SBATCH -n 1\n' +
+        '#SBATCH -c 1\n'+
+        '#SBATCH -C haswell\n'+
+        '#SBATCH -A dessn\n' +
+        '#SBATCH --time=00:09:00\n' +
+        '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_sim.log\n' +
+        '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_sim.log\n' +
+        '#SBATCH --job-name=addcolshal_' + str(i) + '\n' +
+        '#SBATCH --mail-type=NONE\n' +
+        #'#SBATCH --qos=premium\n'+
+        '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
+        '#SBATCH --gres=craynetwork:1\n' +
+        '\n' +
+        'cd /project/projectdirs/des/djbrout/pysmp/\n' +
+        'source setup_scripts/setupcori2.sh\n'+
+        'python corisummarizebigrun.py --filter=' + str(i) + ' \n '
+        '\n'
+    )
+    f.close()
+    output = Popen(["sbatch", script], stdout=PIPE).communicate()
+    print output[0]
