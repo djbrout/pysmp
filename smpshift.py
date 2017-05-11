83b51f2b5aa2458995b6a642192a0ae615bad31f
Dillon Brout <djbrout@gmail.com>
2017-05-11 11:24:55 -0400

    trying to fix skybuff issue

--- smpshift.py
+++ smpshift.py
@@ -5297,7 +5297,7 @@ class smp:
         #mcmc_mag_std = np.array([-999.]*len(xstar))
         mcmc_me_mag_std = np.array([-999.]*len(xstar))
 
-        radius = 8
+        radius = 11
         cntr = 0
         fitrad = np.zeros([substamp,substamp])
         for x in np.arange(substamp):   
