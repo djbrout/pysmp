22380255a7b3b90d469ea5a0408a98d206035bfc
Dillon Brout <djbrout@gmail.com>
2017-05-14 07:13:14 -0400

    trying to fix skybuff issue

--- batch_submission_scripts/loop.py
+++ batch_submission_scripts/loop.py
@@ -3,7 +3,7 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(0,50)
+allindexes = range(250,500)
 filts = ['g','r','i','z']
 #filts = ['g']
 #np.random.shuffle(allindexes)
@@ -20,10 +20,10 @@ for i in allindexes:
             '#SBATCH -c 1\n'+
             '#SBATCH -C haswell\n'+
             '#SBATCH -A dessn\n' +
-            '#SBATCH --time=20:00:00\n' +
-            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sfieldsspec.log\n' +
-            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sfieldsspec.log\n' +
-            '#SBATCH --job-name=spec'+filt+'_' + str(i) + '\n' +
+            '#SBATCH --time=30:00:00\n' +
+            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'x3sim.log\n' +
+            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'x3sim.log\n' +
+            '#SBATCH --job-name=sim'+filt+'_' + str(i) + '\n' +
             '#SBATCH --mail-type=NONE\n' +
             #'#SBATCH --qos=premium\n'+
             '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
@@ -39,9 +39,9 @@ for i in allindexes:
             #'python mpp.py --start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+' \n'
             #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
 
-            'python smpshift.py --index=' + str(i) + ' -f ' + filt + ' --nozpt \n'
-            # 'python smpshift.py --index=' + str(i) + ' -f '+filt+' --nozpt --snfilelist=data/s2lightcurves.txt '
-            #                                                      '-o /project/projectdirs/des/djbrout/116simdeep \n' +
+            #'python smpshift.py --index=' + str(i) + ' -f ' + filt + ' --nozpt \n'
+            'python smpshift.py --index=' + str(i) + ' -f '+filt+' --nozpt --snfilelist=data/s2lightcurves.txt '
+                                                                  '-o /project/projectdirs/des/djbrout/116simdeep \n' +
             '\n'
         )
         f.close()
