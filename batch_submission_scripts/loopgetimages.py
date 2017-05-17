aa5a6e87c742434266a9e68ed9defc865b8e592f
Dillon Brout <djbrout@gmail.com>
2017-05-17 16:16:32 -0400

    trying to fix skybuff issue

---
+++ batch_submission_scripts/loopgetimages.py
@@ -0,0 +1,54 @@
+import os
+from subprocess import *
+import numpy as np
+import time
+
+#allindexes = range(250,500)
+#filts = ['g','r','i','z']
+#filts = ['g']
+#np.random.shuffle(allindexes)
+
+fields = ['S1','S2','X1','X2','X3','C1','C2','C3']#,'E1','E2'
+ccdnums = range(0,63)
+for i in ccdnums:
+    for field in fields:
+        print i
+        script = '/global/cscratch1/sd/dbrout/logs/sm_' + str(i) + '.sh'
+        f = open(script, 'w')
+        f.write(
+            '#!/bin/bash -l\n' +
+            '#SBATCH --partition=shared\n' +
+            '#SBATCH -n 1\n' +
+            '#SBATCH -c 1\n'+
+            '#SBATCH -C haswell\n'+
+            '#SBATCH -A dessn\n' +
+            '#SBATCH --time=00:30:00\n' +
+            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+field+'getim.log\n' +
+            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+field+'getim.log\n' +
+            '#SBATCH --job-name='+field+'_' + str(i) + '\n' +
+            '#SBATCH --mail-type=NONE\n' +
+            #'#SBATCH --qos=premium\n'+
+            '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
+            '#SBATCH --gres=craynetwork:1\n' +
+            '\n' +
+            'cd /project/projectdirs/des/djbrout/pysmp/\n' +
+            'source getimages.sh ' + field + ' '+ i +
+            #'source setup_scripts/setupcori2.sh\n'+
+            #'source /scratch3/scratchdirs/masao/setup_DiffImg.sh\n'
+            #'echo "RUNNING NOW"\n'+
+            #'python test.py\n'
+            #'cd /global/u1/d/dbrout/SEaR/\n' +
+            #'echo "--start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+'" \n'+
+            #'python mpp.py --start='+str(i*nproc)+' --stop='+str((i+1)*nproc)+' \n'
+            #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
+
+            #'python smpshift.py --index=' + str(i) + ' -f ' + filt + ' --nozpt \n'
+            #'python smpshift.py --index=' + str(i) + ' -f '+filt+' --nozpt --snfilelist=data/x3lightcurves.txt '
+            #                                                      '-o /project/projectdirs/des/djbrout/116simdeep '
+            #                                                     '--snfilepath=/project/projectdirs/des/djbrout/pysmp/imglist/all/ \n' +
+            '\n'
+        )
+        f.close()
+        output = Popen(["sbatch", script], stdout=PIPE).communicate()
+        print output[0]
+        #time.sleep(1)
