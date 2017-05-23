f0354ede4857808c6a9bb37113960d9add3a6d26
Dillon Brout <djbrout@gmail.com>
2017-05-23 10:52:39 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -284,9 +284,9 @@ def addtolightcurve(lightcurvefile,saveloc,mjd,flux,fluxerr,zpt,zptrms,chisq,sky
 if __name__ == "__main__":
     print 'started'
     lcdir = '/project/projectdirs/des/djbrout/pysmp/imglist/all/'
-    resultsdir = '/project/projectdirs/des/djbrout/114sim/'
+    resultsdir = '/project/projectdirs/des/djbrout/116simdeep/'
 
-    savelcdir = resultsdir+'/SMP_SIMshallow_v4'
+    savelcdir = resultsdir+'/SMP_SIMdeep_v4'
     fakes = False
     faketrueflux = False
 
