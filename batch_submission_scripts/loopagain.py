f885340a499b459da08cdad345be9a2686450221
Dillon Brout <djbrout@gmail.com>
2017-05-24 16:19:06 -0400

    trying to fix skybuff issue

--- batch_submission_scripts/loopagain.py
+++ batch_submission_scripts/loopagain.py
@@ -2,7 +2,7 @@ import os
 from subprocess import *
 import numpy as np
 import time
-outdir = '/project/projectdirs/des/djbrout/116simdeep/'
+outdir = '/project/projectdirs/des/djbrout/114sim/'
 redofiles = open(outdir+'/missing.txt','r')
 
 #allindexes = range(250,500)
@@ -29,7 +29,7 @@ for i in redofiles:
             '#SBATCH --time=35:00:00\n' +
             '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(fl) + '_'+filt+'.log\n' +
             '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(fl) + '_'+filt+'.log\n' +
-            '#SBATCH --job-name=sid'+filt+'_' + str(fl) + '\n' +
+            '#SBATCH --job-name=sis'+filt+'_' + str(fl) + '\n' +
             '#SBATCH --mail-type=NONE\n' +
             #'#SBATCH --qos=premium\n'+
             '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
