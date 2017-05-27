1d87ccb7f26e9aff0bd9744ba54705f98ed47e99
Dillon Brout <djbrout@gmail.com>
2017-05-27 09:15:23 -0400

    trying to fix skybuff issue

--- tarlc.py
+++ tarlc.py
@@ -47,6 +47,7 @@ readme.write(readmetext)
 readme.close()
 
 os.popen('cd '+savelcdir+'\n ls *.dat > '+savelcdir.split('/')[-1]+'.LIST \n cd .. \n rm '+savelcdir.split('/')[-1]+'.tar.gz \n tar -xf '+savelcdir.split('/')[-1]+'.tar.gz ./'+savelcdir.split('/')[-1]+'\n')
+print ('cd '+savelcdir+'\n ls *.dat > '+savelcdir.split('/')[-1]+'.LIST \n cd .. \n rm '+savelcdir.split('/')[-1]+'.tar.gz \n tar -xf '+savelcdir.split('/')[-1]+'.tar.gz ./'+savelcdir.split('/')[-1]+'\n'))
 #os.popen('rm '+savelcdir.split('/')[-1]+'.tar.gz ')
 #os.popen('cd '+resultsdir+'\n tar -zcf '+savelcdir.split('/')[-1]+'.tar.gz '+savelcdir.split('/')[-1]+'/')
 
