46ad4ef82b302d7e55dfc0a7c27675c1784eae02
Dillon Brout <djbrout@gmail.com>
2017-05-30 00:32:40 -0400

    trying to fix skybuff issue

--- tarlc.py
+++ tarlc.py
@@ -1,7 +1,7 @@
 # resultsdir = '/project/projectdirs/des/djbrout/simv1'
 # savelcdir = '/project/projectdirs/des/djbrout/SMP_SIM_v1'
 resultsdir = '/project/projectdirs/des/djbrout/simv1'
-savelcdir = '/project/projectdirs/des/djbrout/SMP_SIM_v1'
+savelcdir = '/project/projectdirs/des/djbrout/SMP_FAKES_v1'
 
 
 import os
