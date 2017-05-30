d6a92f066214f39a3f07c261223ee09047778dc6
Dillon Brout <djbrout@gmail.com>
2017-05-30 00:11:34 -0400

    trying to fix skybuff issue

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -463,6 +463,7 @@ if __name__ == "__main__":
             zptfile.extend(sndata['ZPTFILE'])
             idobs.extend(sndata['ID_OBS'])
 
+        print 'lcfile',open(lcfile).readlines()
         successful = addtolightcurve(lcfile,savelcfile,mjd,flux,fluxerr,
                      zpt, rmsaddin,
                      chi2,sky,skyerr,smpflag,zptfile,
