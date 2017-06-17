0640d1b5bed358a1fcb08a2a6fa67462da5bc100
Dillon Brout <djbrout@gmail.com>
2017-06-17 18:34:48 -0400

    geweke convergence diagnostic

--- mcmc_geweke.py
+++ mcmc_geweke.py
@@ -1059,6 +1059,21 @@ class metropolis_hastings():
             print(msg)
             hasnotconv = True
 
+
+        if len(np.unique(self.xhistory)) > 1:
+            gw = pymc.geweke(np.array(self.xhistory)[start_iter:], intervals=1, first=.1, last=.5)
+            geweke = np.array(gw)
+            if np.any(np.abs(geweke[:, 1]) > 2.):
+                msg = "Chisq Vec has not properly converged"
+                print(msg)
+                hasnotconv =
+            gw = pymc.geweke(np.array(self.yhistory)[start_iter:], intervals=1, first=.1, last=.5)
+            geweke = np.array(gw)
+            if np.any(np.abs(geweke[:, 1]) > 2.):
+                msg = "Chisq Vec has not properly converged"
+                print(msg)
+                hasnotconv = True
+
         self.modelvec_nphistory = np.zeros((num_iter, len(self.modelvec)))
         for i in np.arange(num_iter):
             self.modelvec_nphistory[ i, : ] = self.modelvechistory[ int(i + start_iter) ]
