be39f74f3110d4bf82fde2f249b4efd2d23a727b
Dillon Brout <djbrout@gmail.com>
2017-06-22 13:32:48 -0400

    geweke convergence diagnostic

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2373,6 +2373,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     for i, sme, sm, ind, r, d, cm in zip(range(len(starmagerr)),starmagerr, starmag, indices, ras, decs, catmag):
         cntr += 1
         if cntr > maxpoints: continue
+        if cntr > 500: continue
         # print starmag[np.isclose(ras,r,rtol=1.e-5) & np.isclose(decs,d,rtol=1.e-5) & (catmag == cm)]
         # print starmag[indices == ind]
         # raw_input()
@@ -2408,7 +2409,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     for sme, sm, ind, r, d, cm,cs in zip(starmagerr, starmag, indices, ras, decs, catmag,chisq):
         cntr += 1
         if cntr > maxpoints: continue
-        if cntr > 50000: continue
+        if cntr > 500: continue
         # print starmag[np.isclose(ras,r,rtol=1.e-5) & np.isclose(decs,d,rtol=1.e-5) & (catmag == cm)]
         # print starmag[indices == ind]
         # raw_input()
@@ -2443,7 +2444,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
     for sme, sm, ind, r, d, cm, cs,se in zip(starmagerr, starmag, indices, ras, decs, catmag, chisq,skyerr):
         cntr += 1
         if cntr > maxpoints: continue
-        if cntr > 50000: continue
+        if cntr > 500: continue
         # print starmag[np.isclose(ras,r,rtol=1.e-5) & np.isclose(decs,d,rtol=1.e-5) & (catmag == cm)]
         # print starmag[indices == ind]
         # raw_input()
