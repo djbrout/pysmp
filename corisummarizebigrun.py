a7bda810ee63991464f95bb5152d6f3e56d5c4c9
Dillon Brout <djbrout@gmail.com>
2017-06-22 15:00:06 -0400

    geweke convergence diagnostic

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1535,7 +1535,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
     ax4.plot(axa, aya, linewidth=3, color='black')
     ax4.plot(axa, aya+aystd, linewidth=2, color='black',linestyle='--')
     ax4.plot(axa, aya-aystd, linewidth=2, color='black',linestyle='--')
-    ax4.set_xlim(ax3.get_xlim())
+    ax4.set_xlim(min(fakemag),27)
     ax4.set_ylim(-.1,.1)
     ax4.set_xlabel('Fake Mag')
     #ax5.set_xlabel('Counts')
