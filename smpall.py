85e59554a8ab7bcd1e87e9261dadd89ff5328096
Dillon Brout <djbrout@gmail.com>
2017-06-29 22:45:59 -0400

    testing sextractor

--- smpall.py
+++ smpall.py
@@ -6127,7 +6127,7 @@ class smp:
                                 #(flux_star_std_mcmc_modelerrors > 1.0) &
                                 (np.isfinite(mag_cat)) &
                                 (np.isfinite(flux_star)) &
-                                (flux_chisq < 100.) &
+                                (flux_chisq < 500.) &
                                 (np.isfinite(flux_chisq)) &
                                 (~np.isnan(flux_chisq)) &
                                 (flux_star > 0) &
