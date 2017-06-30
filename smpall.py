4aeb0b22019ec35449ee499bc63b38608dbeb529
Dillon Brout <djbrout@gmail.com>
2017-06-29 20:50:21 -0400

    testing sextractor

--- smpall.py
+++ smpall.py
@@ -1386,6 +1386,10 @@ class smp:
         for imfile,noisefile,psffile,band, j in \
                 zip(snparams.image_name_search,snparams.image_name_weight,snparams.file_name_psf,snparams.band, range(len(snparams.band))):
             nozpt = copy(orig_nozpt)
+
+            if not '20151029' in imfile:
+                continue
+
             #raw_input('stopped')
             # if round(float(snparams.mjd[j])) != 56935:
             #    continue
