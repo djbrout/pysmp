7793617e3d0c89df994fef07d34cff8b068433ab
Dillon Brout <djbrout@gmail.com>
2017-07-03 17:48:43 -0400

    ready to run test on sim

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -374,6 +374,7 @@ if __name__ == "__main__":
     tsne = []
     for sn in sne:
         if '.smp' in sn:
+            print 'here ',sn
             tsne.append('_'.join(sn.split('_')[:-1]))
     sne = np.array(tsne,dtype='str')
     sne = np.unique(sne)
