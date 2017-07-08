31b0dbf38431bf8b4dea6b14d8166d3fe19dc248
Dillon Brout <djbrout@gmail.com>
2017-07-08 13:22:53 -0400

    debugging badflag saves

--- batch_submission_scripts/loop.py
+++ batch_submission_scripts/loop.py
@@ -3,7 +3,7 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(0,69)
+allindexes = range(0,1800)
 filts = ['g','r','i','z']
 #filts = ['g','z']
 #np.random.shuffle(allindexes)
