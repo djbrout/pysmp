9dde177cb699a35166f23cf96c8efb29f84fdf9c
Dillon Brout <djbrout@gmail.com>
2017-07-11 14:57:35 -0400

    debugging badflag saves

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2291,7 +2291,10 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
         # plt.savefig('stdresid.png')
 
         # plt.clf()
-        ww = fakemag > 90.
+        ww = (fakemag >= 50) & (flux != 0.) & (hostmag < 299999.) & (flux != 0) & (np.array(fakemag, dtype='float') > 0.) \
+         & (fluxerr > 0.) & (np.isfinite(flux)) & \
+         (np.isfinite(fluxerr)) & (~np.isnan(flux)) & (~np.isnan(fluxerr)) & (chisqarr > .2) \
+         & (chisqarr < 1.4)
         ax1.scatter(hostmag[ww], d[ww], alpha=.3, color='blue')
         ax, ay, aystd = dt.bindata(hostmag[ww], d[ww], np.arange(min(hostmag), 27.5, .1), window=1.5)
         ax1.plot([min(hostmag), max(hostmag)], [0, 0], color='grey')
