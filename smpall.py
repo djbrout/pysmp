979acf0569e61d91b5e7ad5d5d7c8ff40a4addcd
Dillon Brout <djbrout@gmail.com>
2017-07-13 00:28:06 -0400

    debugging badflag saves

--- smpall.py
+++ smpall.py
@@ -1936,16 +1936,16 @@ class smp:
 
 
             i += 1
+
             try:
                 smp_dict['image_filename'][i] = imfile
                 smp_dict['zpt_file'][i] = 'na'
                 smp_dict['psf_filename'][i] = psffile
                 smp_dict['weight_filename'][i] = weightsfile
             except:
-                smp_dict['image_filename'][i] = 'na'
-                smp_dict['zpt_file'][i] = 'na'
-                smp_dict['psf_filename'][i] = 'na'
-                smp_dict['weight_filename'][i] = 'na'
+                print 'ranoutofroom'*100
+                badflag = 1
+                descriptiveflag = 128
 
             if xsn < 0 or ysn < 0 or xsn > snparams.nxpix-1 or ysn > snparams.nypix-1:
                 print "Error : SN Coordinates %s,%s are not within image"%(snparams.ra,snparams.decl)
