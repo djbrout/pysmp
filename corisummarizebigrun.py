38aacd2d6c3a87e73b9bc9c9bf524e1e36892a0f
Dillon Brout <djbrout@gmail.com>
2017-07-14 13:32:41 -0400

    debugging badflag saves

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1607,7 +1607,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
                                     np.arange(20., 26., .1),window=2.,dontrootn=True)
             ax4.plot([19, 28.7], [0, 0],color='grey')
 
-            ax, ayrms = dt.binrms(fakemag[ww][d[ww]<3.], d[ww][d[ww]<3.], np.arange(20., 28, .1), 1.5)
+            ax, ayrms = dt.binrms(fakemag[ww][d[ww]<10.], d[ww][d[ww]<10.], np.arange(20., 28, .1), 1.5)
             ax3.plot(ax, ayrms, color=col, label=filt+' band', linewidth=3,alpha=.8)
             ax3.plot(ax, ax * 0 + 1., linestyle='--', color='black',alpha=.8)
             ax4.plot(axa, aya, linewidth=3, color=col,label=filt+' band',alpha=.8)
