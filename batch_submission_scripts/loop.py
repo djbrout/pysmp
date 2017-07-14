b379903652a8155d14b808cccda1a94247223f23
Dillon Brout <djbrout@gmail.com>
2017-07-14 08:45:18 -0400

    debugging badflag saves

--- batch_submission_scripts/loop.py
+++ batch_submission_scripts/loop.py
@@ -3,16 +3,16 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(0,300)
+allindexes = range(0,410)
 #allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
 filts = ['g','r','i','z']
-filts = ['r']
+#filts = ['r']
 walltime= '48:00:00'
 #np.random.shuffle(allindexes)
 
 doskipping = True
-snfilelist = 'data/x3lightcurves.txt'
-outdir = '/project/projectdirs/des/djbrout/simtestx3'
+snfilelist = 'data/allspec.txt'
+outdir = '/project/projectdirs/des/djbrout/specv1_1'
 snfiles = open(snfilelist).readlines()
 for i in allindexes:
     for filt in filts:
@@ -36,9 +36,9 @@ for i in allindexes:
             '#SBATCH -C haswell\n'+
             '#SBATCH -A des\n' +
             '#SBATCH --time='+walltime+'\n' +
-            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sim2d.log\n' +
-            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sim2d.log\n' +
-            '#SBATCH --job-name=smd_'+filt+'_' + str(i) + '\n' +
+            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'spec2.log\n' +
+            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'spec2.log\n' +
+            '#SBATCH --job-name=spc_'+filt+'_' + str(i) + '\n' +
             '#SBATCH --mail-type=NONE\n' +
             #'#SBATCH --qos=premium\n'+
             '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
@@ -55,7 +55,7 @@ for i in allindexes:
             #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
             'export WALLTIME='+walltime.split(':')[0]+'\n'+
             'python smpall.py --index=' + str(i) + ' -f ' + filt +
-            ' -o '+outdir+' --snfilelist='+snfilelist+' --usefake '+
+            ' -o '+outdir+' --snfilelist='+snfilelist+' '+
             '--snfilepath=/project/projectdirs/des/djbrout/pysmp/imglist/all/ \n'
 
 
