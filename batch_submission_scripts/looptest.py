15c888a4c7c65f60a79bb17baee803db6820e527
Dillon Brout <djbrout@gmail.com>
2017-07-15 11:03:02 -0400

    making npz files

--- batch_submission_scripts/looptest.py
+++ batch_submission_scripts/looptest.py
@@ -3,7 +3,7 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(1100,1200)
+allindexes = range(1106,1200)
 #allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
 filts = ['g','r','i','z']
 #filts = ['r']
