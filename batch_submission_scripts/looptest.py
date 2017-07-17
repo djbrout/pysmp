352c4ea5a947b6894fd0b4b120a80483d0b844a6
Dillon Brout <djbrout@gmail.com>
2017-07-17 01:48:34 -0400

    making npz files

--- batch_submission_scripts/looptest.py
+++ batch_submission_scripts/looptest.py
@@ -3,16 +3,16 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(1106,1200)
+allindexes = range(0,410)
 #allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
 filts = ['g','r','i','z']
 #filts = ['r']
-walltime= '00:30:00'
+walltime= '00:40:00'
 #np.random.shuffle(allindexes)
 
 doskipping = True
-snfilelist = 'data/s2lightcurves.txt'
-outdir = '/project/projectdirs/des/djbrout/dummytest2'
+snfilelist = 'data/allspec.txt'
+outdir = '/project/projectdirs/des/djbrout/specdummytest2'
 snfiles = open(snfilelist).readlines()
 for i in allindexes:
     for filt in filts:
@@ -36,8 +36,8 @@ for i in allindexes:
             '#SBATCH -C haswell\n'+
             '#SBATCH -A des\n' +
             '#SBATCH --time='+walltime+'\n' +
-            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sim2s.log\n' +
-            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sim2s.log\n' +
+            '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'spec2s.log\n' +
+            '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'spec2s.log\n' +
             '#SBATCH --job-name=preps_'+filt+'' + str(i) + '\n' +
             '#SBATCH --mail-type=NONE\n' +
             #'#SBATCH --qos=premium\n'+
@@ -55,7 +55,7 @@ for i in allindexes:
             #'python mpp.py --start=' + str(i * nproc) + ' --stop=' + str((i + 1) * nproc) + ' \n'
             'export WALLTIME='+walltime.split(':')[0]+'\n'+
             'python smpall.py --index=' + str(i) + ' -f ' + filt +
-            ' -o '+outdir+' --snfilelist='+snfilelist+' --usefake '+
+            ' -o '+outdir+' --snfilelist='+snfilelist+' '+
             '--snfilepath=/project/projectdirs/des/djbrout/pysmp/imglist/all/ \n'
 
 
