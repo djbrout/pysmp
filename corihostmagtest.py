12f6aa5013cad89a7decafd0b4b11fb816069c58
Dillon Brout <djbrout@gmail.com>
2017-07-20 17:39:52 -0400

    making npz files

--- corihostmagtest.py
+++ corihostmagtest.py
@@ -2314,6 +2314,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
         sarr = ['s10', 's20', 's30', 's40', 's50', 's60', 's70', 's80', 's90', 's100']
         parr = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.]
         for p, m, s in zip(parr, marr, sarr):
+            print bigdata[m].shape
+            raw_input('mmm')
             trms = (bigdata[m] - fakeflux) / bigdata[sarr]
             ax, ayrms = dt.binrms(hostmag[ww], trms[ww], np.arange(min(hostmag[ww]), max(hostmag[ww]), .1), .5)
             ax3.plot(ax, ayrms, label=m, linewidth=3)
