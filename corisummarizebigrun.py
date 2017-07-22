f70b46edb73cd7907c48a2ec9976396049f5c1a4
Dillon Brout <djbrout@gmail.com>
2017-07-21 23:32:18 -0400

    making npz files

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -3504,15 +3504,15 @@ if __name__ == "__main__":
 
     #resultsdir = '/pnfs/des/scratch/pysmp/smp_04_modelerrors'
     #resultsdir = '/pnfs/des/scratch/pysmp/smp_02_simnosnnoskyerr'
-    resultsdir = '/project/projectdirs/des/djbrout/specv1_1/'
-    resultsdir = '/project/projectdirs/des/djbrout/simtest/'
+    resultsdir = '/project/projectdirs/dessn/dbrout/specv1_1/'
+    #resultsdir = '/project/projectdirs/des/djbrout/simtest/'
     #resultsdir= './working/'
     #resultsdir= '/export/scratch0/ps1sn1/data/v10.0/GPC1v3/eventsv1/smpworkspace/PS_TEST1/'
     #resultsdir = './workingsimnosn'
     isfermigrid = False
     cacheddata = False
 
-    deep_or_shallow = 'shallow'
+    deep_or_shallow = 'spec'
 
     #cd = '/project/projectdirs/des/djbrout/v67pixshift//summary_results.npz'
     #cd = '/pnfs/des/scratch/pysmp/smp_02_simnosnnoskyerr/np_data/summary_results.npz'
