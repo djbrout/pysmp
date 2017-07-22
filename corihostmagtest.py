59a329e5a6b43faeb465f8544c2660c1b03a4809
Dillon Brout <djbrout@gmail.com>
2017-07-22 17:11:11 -0400

    making npz files

--- corihostmagtest.py
+++ corihostmagtest.py
@@ -3629,8 +3629,8 @@ if __name__ == "__main__":
 
     #resultsdir = '/pnfs/des/scratch/pysmp/smp_04_modelerrors'
     #resultsdir = '/pnfs/des/scratch/pysmp/smp_02_simnosnnoskyerr'
-    resultsdir = '/project/projectdirs/dessn/dbrout/specv1_1/'
-    #resultsdir = '/project/projectdirs/des/djbrout/simtest/'
+    ##resultsdir = '/project/projectdirs/dessn/dbrout/specv1_1/'
+    resultsdir = '/project/projectdirs/des/djbrout/simtest/'
     #resultsdir= './working/'
     #resultsdir= '/export/scratch0/ps1sn1/data/v10.0/GPC1v3/eventsv1/smpworkspace/PS_TEST1/'
     #resultsdir = './workingsimnosn'
@@ -3683,4 +3683,4 @@ if __name__ == "__main__":
 
     #print cd
     #raw_input()
-    go(fakedir,resultsdir,cacheddata,cd,filter,tfield,dostars,deep_or_shallow,real=True)
+    go(fakedir,resultsdir,cacheddata,cd,filter,tfield,dostars,deep_or_shallow,real=False)
