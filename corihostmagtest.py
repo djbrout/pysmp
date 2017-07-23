b20dab1b5ce2288d8a743c0f9cb24d6a8d85417c
Dillon Brout <djbrout@gmail.com>
2017-07-23 15:24:25 -0400

    making npz files

--- corihostmagtest.py
+++ corihostmagtest.py
@@ -762,6 +762,7 @@ def grabdata(tmpwriter,resultsdir,cd,tfield,filter = 'g',oldformat=False,real=Fa
 
         for jd in mmjd:
             print data['MJD']
+            print jd
             w = data['MJD'] == jd
             if len(data['FAKEMAG'][w]) == 0:
                 bigdata['bootfakemag'].extend([99])
