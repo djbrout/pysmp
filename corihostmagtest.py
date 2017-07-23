8763a53164ff895136a6563e8e62d6ec9656c62b
Dillon Brout <djbrout@gmail.com>
2017-07-23 17:15:23 -0400

    making npz files

--- corihostmagtest.py
+++ corihostmagtest.py
@@ -1520,7 +1520,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
     print 'm100',bigdata['m100'][:100]
     print 's100',bigdata['s100'][:100]
     raw_input('asdf')
-    myww = (bigdata['bootdpmjd'] > -35.) & (bigdata['bootdpmjd'] < 230.) & (np.asarray(bigdata['bootfakemag']) > 0.) & (np.asarray(bigdata['bootfitzpt']) != 99.) (np.asarray(bigdata['bootfitzpt']) != 0.) & (np.asarray(bigdata['m100']) != 0) & (np.asarray(bigdata['s100'])!=0.)
+    myww = (bigdata['bootdpmjd'] > -35.) & (bigdata['bootdpmjd'] < 230.) & (np.asarray(bigdata['bootfakemag']) > 0.) & (np.asarray(bigdata['bootfitzpt']) != 99.) & (np.asarray(bigdata['bootfitzpt']) != 0.) & (np.asarray(bigdata['m100']) != 0) & (np.asarray(bigdata['s100'])!=0.)
     marr = ['m1', 'm2', 'm3', 'm4', 'm5', 'm10', 'm20', 'm50', 'm100']
     sarr = ['s1', 's2', 's3', 's4', 's5', 's10', 's20', 's50', 's100']
     parr = [.01, .02, .03, .04, .05, .1, .2, .5, 1.]
