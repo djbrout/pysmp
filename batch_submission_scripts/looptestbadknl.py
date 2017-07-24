bf5dd07bbc457a380ccef5b8f644c4fc28ce09f4
Dillon Brout <djbrout@gmail.com>
2017-07-24 18:29:30 -0400

    making npz files

---
+++ batch_submission_scripts/looptestbadknl.py
@@ -0,0 +1,71 @@
+import os
+from subprocess import *
+import numpy as np
+import time
+
+allindexes = range(0,40)
+#allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
+filts = ['g','r','i','z']
+#filts = ['r']
+walltime= '10:00:00'
+#np.random.shuffle(allindexes)
+
+doskipping = True
+snfilelist = 'badinputs.txt'
+#snfilelist = 'data/s2lightcurves.txt'
+outdir = '/project/projectdirs/dessn/dbrout/simdummytest3'
+snfiles = open(snfilelist).read()
+snfiles = snfiles.split('.smp')
+
+text = '#!/bin/bash -l\n#SBATCH --partition=shared\n' \
+       '#SBATCH -n 1\n#SBATCH -c 1\n#SBATCH -C knl,quad,flat\n' \
+       '#SBATCH -A m2875\n' \
+       '#SBATCH --time=' + walltime + '\n' + \
+       '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/knldummy.log\n' + \
+       '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/knldummy.log\n' + \
+       '#SBATCH --job-name=preps1\n' + \
+       '#SBATCH --mail-type=NONE\n' + \
+       '#SBATCH --mail-user=bdrizzle@yahoo.com\n' + \
+       '#SBATCH --gres=craynetwork:1\n' + \
+       '\n' + \
+       'cd /project/projectdirs/des/djbrout/pysmp/\n' + \
+       'source setup_scripts/setupcori2.sh\n' + \
+       'echo "RUNNING NOW"\n' + \
+       'export WALLTIME=' + walltime.split(':')[0] + '\n'
+
+for i in allindexes:
+    if snfiles[i] == '\n': continue
+    filt = snfiles[i].split('_')[-1]
+    snfiles[i] = snfiles[i][:-2]+'.dat'
+    #print snfiles[i]
+    #raw_input()
+    #for filt in filts:
+    if True:
+        if doskipping:
+            print snfiles[i]
+            sn = snfiles[i].split('/')[-1].split('.')[0]
+            # if os.path.exists(outdir+'/lightcurves/'+sn+'_'+filt+'.smp'):
+            #     print 'skipping ',outdir+'/lightcurves/'+sn+'_'+filt+'.smp  because already exists a good fit...'
+            #     continue
+            # if os.path.exists('/global/cscratch1/sd/dbrout/specnpzfiles/'+sn+'_'+filt+'.mcmcinput.npz'):
+            #     print 'skipping ', outdir + '/lightcurves/' + sn + '_' + filt + '.smp  because already exists a good fit...'
+            #     continue
+            # else:
+            #     print 'nope',outdir+'/lightcurves/'+sn+'_'+filt+'.smp'
+            #     continue
+        print i
+
+
+        text += 'python smptest.py -f ' + filt +\
+            ' -o '+outdir+' -s /project/projectdirs/des/djbrout/pysmp/imglist/all/'+snfiles[i]+' '\
+            '--snfilepath=/project/projectdirs/des/djbrout/pysmp/imglist/all/ & \n'\
+            '\n'\
+
+text += 'wait \n \n \n'
+
+script = '/global/cscratch1/sd/dbrout/logs/knlprep.sh'
+f = open(script, 'w')
+f.write(text)
+
+output = Popen(["sbatch", script], stdout=PIPE).communicate()
+print output[0]
