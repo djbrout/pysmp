087be2df58f0e8995d289639c144dda7b8ca922a
Dillon Brout <djbrout@gmail.com>
2017-07-25 11:35:40 -0400

    making npz files

--- batch_submission_scripts/looptestbadknl.py
+++ batch_submission_scripts/looptestbadknl.py
@@ -3,7 +3,7 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(75,125)
+allindexes = range(75,100)
 filts = ['g','r','i','z']
 walltime= '00:30:00'
 
@@ -15,10 +15,10 @@ snfiles = open(snfilelist).readlines()
 
 text = '#!/bin/bash -l\n#SBATCH --partition=debug\n' \
        '#SBATCH -N 1\n#SBATCH -C knl\n' \
-       '#SBATCH -A m2875\n' \
+       '#SBATCH -A des\n' \
        '#SBATCH --time=' + walltime + '\n' + \
-        '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/knldebugdummy4.log\n' + \
-       '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/knldebugdummy4.log\n' + \
+        '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/debugdummy4.log\n' + \
+       '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/debugdummy4.log\n' + \
        '#SBATCH --job-name=preps1\n' + \
        '#SBATCH --mail-type=NONE\n' + \
        '#SBATCH --mail-user=bdrizzle@yahoo.com\n' + \
