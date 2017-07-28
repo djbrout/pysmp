7efd6698f0a15c7c649e7aed47b7d7f13ba9020c
Dillon Brout <djbrout@gmail.com>
2017-07-28 14:19:59 -0400

    making npz files

--- smptest.py
+++ smptest.py
@@ -278,7 +278,7 @@ class smp:
         self.rootdir = rootdir
         self.psf_model = psf_model
 
-    #@profile
+    @profile
     def main(self,nodiff=False,nozpt=False,rootdir='',outdir='' ,
              nomask=False,outfile='',debug=False,
              verbose=False, clear_zpt=False,clear_checkstars=True,mergeno=0,
