f7433c7ae3a3b2b9074831d41cc8cfac373b4f96
Dillon Brout <djbrout@gmail.com>
2017-07-28 12:59:50 -0400

    making npz files

--- batch_submission_scripts/looptest.py
+++ batch_submission_scripts/looptest.py
@@ -42,6 +42,7 @@ for i in allindexes:
             '#SBATCH --partition=shared\n' +
             '#SBATCH -n 1\n' +
             '#SBATCH -c 1\n'+
+            '#SBATCH --mem=2.5GB\n'+
             #'#SBATCH -C haswell\n'+
             '#SBATCH -A des\n' +
             '#SBATCH --time='+walltime+'\n' +
