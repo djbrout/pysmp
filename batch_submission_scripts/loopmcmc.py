e5b275eb77532ba69f579e79b77ac88052dabe70
Dillon Brout <djbrout@gmail.com>
2017-08-01 23:41:10 -0400

    making npz files

--- batch_submission_scripts/loopmcmc.py
+++ batch_submission_scripts/loopmcmc.py
@@ -3,7 +3,7 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(0,40)
+allindexes = range(40,1350)
 #allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
 #filts = ['g','r','i','z']
 #filts = ['r']
