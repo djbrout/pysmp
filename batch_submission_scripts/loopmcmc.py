58d46f6c74b43619aa66fcadb20dfe95cefe98bb
Dillon Brout <djbrout@gmail.com>
2017-08-04 13:44:41 -0400

    making npz files

--- batch_submission_scripts/loopmcmc.py
+++ batch_submission_scripts/loopmcmc.py
@@ -3,11 +3,11 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(420,421)
+allindexes = range(0,2000)
 #allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
 #filts = ['g','r','i','z']
 #filts = ['r']
-walltime= '15:00:00'
+walltime= '30:00:00'
 #np.random.shuffle(allindexes)
 
 doskipping = False
