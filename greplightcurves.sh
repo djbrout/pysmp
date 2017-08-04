1c9c43106c0f1dbae5645a619d7d0b85fa047969
Dillon Brout <djbrout@gmail.com>
2017-08-03 23:42:26 -0400

    making npz files

--- greplightcurves.sh
+++ greplightcurves.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-awk 'FNR > 50 { nextfile }; /SN-X3/ { print FILENAME }' /project/projectdirs/des/djbrout/pysmp/imglist/all/des_fake_*.dat | uniq > data/x3lightcurves.txt
+awk 'FNR > 50 { nextfile }; /SN-X1/ { print FILENAME }' /project/projectdirs/des/djbrout/pysmp/imglist/all/des_fake_*.dat | uniq > data/x1lightcurves.txt
