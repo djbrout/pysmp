bfa76ac843119b9391440b9fe3df3b7594bfb188
Dillon Brout <djbrout@gmail.com>
2017-08-07 00:22:01 -0400

    making npz files

--- batch_submission_scripts/loopmcmc.py
+++ batch_submission_scripts/loopmcmc.py
@@ -171,7 +171,7 @@ for i in allindexes:
             '#SBATCH --partition=shared\n' +
             '#SBATCH -n 1\n' +
             '#SBATCH -c 1\n'+
-            #'#SBATCH -C haswell\n'+
+            '#SBATCH -C haswell\n'+
             '#SBATCH -A des\n' +
             '#SBATCH --time='+walltime+'\n' +
             '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_mcmcspec.log\n' +
