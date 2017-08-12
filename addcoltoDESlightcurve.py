e78f681e01af5dfb110ca122824ad5b5d4bdbbf7
Dillon Brout <djbrout@gmail.com>
2017-08-12 15:55:17 -0400

    making npz files

--- addcoltoDESlightcurve.py
+++ addcoltoDESlightcurve.py
@@ -448,7 +448,7 @@ if __name__ == "__main__":
     for sn in sne[::-1]:
         if dodiffim:
             os.popen('cp '+lcdir+'/'+sn.split('.')[0]+'.dat '+savelcdir+'/')
-            donesne.append(sn)
+            donesne.append(sn.split('.dat')[0].split('_')[-1])
             continue
         mjd = []
         flux = []
