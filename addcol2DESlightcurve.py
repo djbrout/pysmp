511d6923955ac0ef832129564ebfaf883b415b28
Dillon Brout <djbrout@gmail.com>
2017-09-20 00:14:34 -0400

    printing masked pixels

--- addcol2DESlightcurve.py
+++ addcol2DESlightcurve.py
@@ -390,7 +390,7 @@ if __name__ == "__main__":
     #raw_input()
 
     if savelcdir is None:
-        savelcdir = resultsdir + '/SMP_SPEC3'
+        savelcdir = resultsdir + '/SMP_SPEC4'
 
     if not os.path.exists(os.path.basename(savelcdir)):
         os.mkdir(os.path.basename(savelcdir))
