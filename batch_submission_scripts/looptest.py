eb24b392bf5df4a2f0c5c1192013bd7777726d1b
Dillon Brout <djbrout@gmail.com>
2017-09-22 09:15:22 -0400

    printing masked pixels

--- batch_submission_scripts/looptest.py
+++ batch_submission_scripts/looptest.py
@@ -3,14 +3,14 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = range(97,98)
+allindexes = range(0,97)
 #allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
-#filts = ['g','r','i','z']
-filts = ['z']
+filts = ['g','r','i','z']
+#filts = ['z']
 walltime= '5:10:00'
 #np.random.shuffle(allindexes)
 
-doskipping = False
+doskipping = True
 #snfilelist = 'badinputs.txt'
 #snfilelist = 'data/s1lightcurves.txt'
 snfilelist = 'data/speclist.txt'
