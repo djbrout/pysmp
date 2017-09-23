f229a5dea0e872b4ef3bb8afeb7dcf085c99b915
Dillon Brout <djbrout@gmail.com>
2017-09-23 15:33:36 -0400

    fixing bad exit

--- addcol2DESlightcurve.py
+++ addcol2DESlightcurve.py
@@ -318,7 +318,7 @@ def addtolightcurve(lightcurvefile,saveloc,mjd,flux,fluxerr,zpt,zptrms,chisq,sky
                                        ' '+str(round(flux_zpt, 3))+' '+str(round(fit_zpt, 3))+' '+str(round(fit_zpt_std, 3))+ \
                                        ' '+str(round(chisq[ww][0], 3))+ \
                                        ' ' + str(round(tsky, 3)) + ' ' + str(round(tskyerr, 3)) + \
-                                       ' ' + str(fix[ww][0]) + ' ' + str(int(dflag[ww][0]))+ ' ' + str(int(gain[ww][0])) + '\n'
+                                       ' ' + str(fix[ww][0]) + ' ' + str(int(dflag[ww][0]))+ ' ' + str(float(round(gain[ww][0],3))) + '\n'
                                 #print len(wline.split())
                             #raw_input()
                             #print wline
