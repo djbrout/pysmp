3c50e499ecd33016e5c2f918f71fe701e1058819
Dillon Brout <djbrout@gmail.com>
2017-09-23 21:54:42 -0400

    getting rid of star global offsets

--- batch_submission_scripts/looptest.py
+++ batch_submission_scripts/looptest.py
@@ -12,7 +12,7 @@ walltime= '12:10:00'
 
 doskipping = True
 #snfilelist = 'badinputs.txt'
-snfilelist = 'data/e1lightcurves.txt'
+snfilelist = 'data/e2lightcurves.txt'
 #snfilelist = 'data/speclist.txt'
 
 outdir = '/project/projectdirs/dessn/dbrout/simv2.0/'
@@ -46,7 +46,7 @@ for ii in allindexes:
             #     continue
         print snfiles[int(i)],'submitted'
         count += 1
-        if count < 94: continue
+        if count < 78: continue
         print count, tot
         #continue
         script = '/global/cscratch1/sd/dbrout/logs/sm_' + str(i) + '.sh'
@@ -61,7 +61,7 @@ for ii in allindexes:
             '#SBATCH --time='+walltime+'\n' +
             '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sims2.log\n' +
             '#SBATCH --error=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sims2.log\n' +
-            '#SBATCH --job-name=e1_'+filt+'' + str(i) + '\n' +
+            '#SBATCH --job-name=e2_'+filt+'' + str(i) + '\n' +
             '#SBATCH --mail-type=NONE\n' +
             #'#SBATCH --qos=premium\n'+
             '#SBATCH --mail-user=bdrizzle@yahoo.com\n' +
