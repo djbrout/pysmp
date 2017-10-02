59e4ffba8a527243be147a9c2f166770faf3b6ad
Dillon Brout <djbrout@gmail.com>
2017-10-01 22:44:41 -0400

    getting rid of star global offsets

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -3628,6 +3628,7 @@ def plotstarlc(flux,fluxerr,zpt,ids,mjd,catmag):
             #print 'saved'
         #axs.ravel()[int(i%16)].errorbar(np.array(mjd[ww],dtype='float'),flux[ww]*10**(-.4*(31.-zpt[ww])),yerr=fluxerr[ww]*10**(-.4*(31-zpt[ww])),fmt='o',color='black')
     pdf_pages.close()
+
     #plt.savefig('allstarlc.png')
     print 'saved allstarlc.pdf'
 
