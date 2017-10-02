7ec6f0624bbb251c59392e46e9e03b2add50814a
Dillon Brout <djbrout@gmail.com>
2017-10-02 14:52:13 -0400

    getting rid of star global offsets

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2876,7 +2876,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
 
     maxpoints = 5000000
 
-    load = True
+    load = False
     if load:
         a = np.load(outdir + '/pltstarvec.npz')
         pltvecy = a['pltvecy']
