1e71d47b626310a840b7b1f56a26874929a5e749
Dillon Brout <djbrout@gmail.com>
2017-10-20 12:30:56 -0400

    checking skyerr

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -402,7 +402,7 @@ def grabdata(tmpwriter,resultsdir,cd,tfield,filter = 'g',oldformat=False,real=Fa
 
     import pandas as pd
 
-    dofakedata = pd.read_csv('data/doFake.out', delim_whitespace=True, header=0)
+    dofakedata = pd.read_csv('data/pareddoFakeg.out', delim_whitespace=True, header=0)
     # print dofakedata
     dofakeexpnum = dofakedata['EXPNUM'].values
     dofakemag2 = dofakedata['TRUEMAG'].values
