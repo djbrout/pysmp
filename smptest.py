cb94a2af76f4d73a1b0e8c4abe8c32d79ea879a0
Dillon Brout <djbrout@gmail.com>
2017-10-20 07:57:23 -0400

    checking skyerr

--- smptest.py
+++ smptest.py
@@ -3112,8 +3112,8 @@ class smp:
                                     #raw_input('checking')
                                     smp_dict['aper_skyerr'][i] = aperskyerr
                                     smp_dict['aperscale'][i] = aperscale
-
-
+                                    print 'checkinggg'
+                                    print sexsky,skyx
 
                                     smp_dict['imwcs'].append(w)
                                     msk = copy(image_stamp)
@@ -6209,7 +6209,7 @@ class smp:
                         #temp, skysig, vals = sigma_clip.meanclip(im[ylow:yhi, xlow:xhi]-bkgrnd[ylow:yhi,xlow:xhi], clipsig=4, maxiter=8)
 
                         mag1, magerr1, flux1, fluxerr1, sky1, skysig, badflag1, outstr1 = \
-                            aper.aper(im, y, x, apr=params.fitrad, verbose=False)
+                            aper.aper(im, x, y, apr=params.fitrad, verbose=False)
                         print sky1,skysig
                         #raw_input('sss')
                         sein = skysig
