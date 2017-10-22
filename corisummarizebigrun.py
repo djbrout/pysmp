af3c874ba81dae68dd05f663e1176066ed854371
Dillon Brout <djbrout@gmail.com>
2017-10-21 22:38:57 -0400

    checking skyerr

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -458,9 +458,9 @@ def grabdata(tmpwriter,resultsdir,cd,tfield,filter = 'g',oldformat=False,real=Fa
         deep = 0
         #os.system('cp '+f+' test.npz')
         data = dt.readcol(f)
-        #print data.keys()
+        print data.keys()
         #print data['PSF_FILE']
-        #raw_input('stop')
+        raw_input('stop')
 
         #print tra
         try:
@@ -1991,7 +1991,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
 
     sig = 1.48 * np.median(np.abs(d[(abs(d) < 3.)&(fakemag>30)&(hostmag >26.)]))
     mean = np.mean(d[(abs(d) < 3.) & (fakemag > 30) & (hostmag > 26.)])
-    plt.hist((flux[(fakemag==99.)&(hostmag >26.)]-fakeflux[(fakemag>50)&(hostmag >26.)])/fluxerr[(fakemag>50)&(hostmag >26.)],bins=np.arange(-5.1,5,.2),label='Median:%.3f\nSigma %.2f'%(mean,sig),normed=True)
+    plt.hist((flux[(fakemag>50.)&(hostmag >26.)]-fakeflux[(fakemag>50)&(hostmag >26.)])/fluxerr[(fakemag>50)&(hostmag >26.)],bins=np.arange(-5.1,5,.2),label='Median:%.3f\nSigma %.2f'%(mean,sig),normed=True)
     for s in np.unique(snid[(fakemag>50)&(hostmag >26.)&(d>3.5)&(d<5.)]):
         if s == 226296:
             for fm,m in zip(mjd[(fakemag>50)&(hostmag >26.)&(d>3.5)&(d<5.)&(snid==s)],fakemag[(fakemag>50)&(hostmag >26.)&(d>3.5)&(d<5.)&(snid==s)]):
