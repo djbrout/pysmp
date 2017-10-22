c2dac5e182eb10c8a07d960faccf1d2432bbacd8
Dillon Brout <djbrout@gmail.com>
2017-10-21 22:33:11 -0400

    checking skyerr

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1991,7 +1991,7 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
 
     sig = 1.48 * np.median(np.abs(d[(abs(d) < 3.)&(fakemag>30)&(hostmag >26.)]))
     mean = np.mean(d[(abs(d) < 3.) & (fakemag > 30) & (hostmag > 26.)])
-    plt.hist((flux[(fakemag=99.)&(hostmag >26.)]-fakeflux[(fakemag>50)&(hostmag >26.)])/fluxerr[(fakemag>50)&(hostmag >26.)],bins=np.arange(-5.1,5,.2),label='Median:%.3f\nSigma %.2f'%(mean,sig),normed=True)
+    plt.hist((flux[(fakemag==99.)&(hostmag >26.)]-fakeflux[(fakemag>50)&(hostmag >26.)])/fluxerr[(fakemag>50)&(hostmag >26.)],bins=np.arange(-5.1,5,.2),label='Median:%.3f\nSigma %.2f'%(mean,sig),normed=True)
     for s in np.unique(snid[(fakemag>50)&(hostmag >26.)&(d>3.5)&(d<5.)]):
         if s == 226296:
             for fm,m in zip(mjd[(fakemag>50)&(hostmag >26.)&(d>3.5)&(d<5.)&(snid==s)],fakemag[(fakemag>50)&(hostmag >26.)&(d>3.5)&(d<5.)&(snid==s)]):
