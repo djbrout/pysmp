d8dc9ce3cf293dfb7a25f1ca6eb695697baf1cef
Dillon Brout <djbrout@gmail.com>
2017-10-24 22:39:54 -0400

    need to get ra and dec delta for each night and append to bigdata

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -1995,7 +1995,8 @@ def plotsigmaresid(flux,fluxerr,fakemag,fitzpt,fakezpt,hostmag,chisqarr,rmsaddin
                                      np.arange(20., 26., .1), window=2., dontrootn=True)
         #ax4.plot([19, 28.7], [0, 0], color='grey')
 
-        ax, ayrms = dt.binrms(hostmag[(d < 3.)&(fakemag>50)], d[(d < 3.)&(fakemag>50)], np.arange(20., 28, .5), .5)
+        ax, ayrms = dt.binrms(hostmag[(d < 3.)&(fakemag>50)&((dpmjd>200)|(dpmjd<-40))],
+                              d[(d < 3.)&(fakemag>50)&((dpmjd>200)|(dpmjd<-40))], np.arange(20., 28, .5), .5)
         ax3.plot(ax, ayrms, color='blue', label='ALL SNe', linewidth=3)
 
         ax3.plot(ax, ax * 0 + 1., linestyle='--', color='black')
