fcb7c0b7252ed3278667469535b25abcf64588d6
Dillon Brout <djbrout@gmail.com>
2017-10-30 11:20:59 -0400

    need to get ra and dec delta for each night and append to bigdata

--- corisummarizebigrun.py
+++ corisummarizebigrun.py
@@ -2927,7 +2927,7 @@ def plotstarrms(flux,fluxerr,zpt,catmag,chisq,rmsaddin,sky,skyerr,poisson,indice
 
     maxpoints = 5000000
 
-    load = True
+    load = False
     if load:
         a = np.load(outdir + '/pltstarvec.npz')
         pltvecy = a['pltvecy']
