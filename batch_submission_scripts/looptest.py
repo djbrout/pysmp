d08ca7f6e217a48721a5e50db8b8e9d947d0cbad
Dillon Brout <djbrout@gmail.com>
2017-10-31 03:46:57 -0400

    need to get ra and dec delta for each night and append to bigdata

--- batch_submission_scripts/looptest.py
+++ batch_submission_scripts/looptest.py
@@ -3,7 +3,9 @@ from subprocess import *
 import numpy as np
 import time
 
-allindexes = np.arange(0,2500)
+allindexes = np.arange(2313,2500)
+allindexes = np.arange(10000,12500)
+
 #allindexes = np.arange(362,4089,1)
 #allindexes = [100,107,113,120,13,178,214,269,278,40,60,80,92]
 filts = ['g','r','i','z']
@@ -60,7 +62,7 @@ for ii in allindexes:
             '#SBATCH --partition=shared\n' +
             '#SBATCH -n 1\n' +
             '#SBATCH -c 1\n'+
-            '#SBATCH -C haswell\n'+
+            #'#SBATCH -C haswell\n'+
             '#SBATCH -A dessn\n' +
             '#SBATCH --time='+walltime+'\n' +
             '#SBATCH --output=/global/cscratch1/sd/dbrout/logs/' + str(i) + '_'+filt+'sims2.log\n' +
