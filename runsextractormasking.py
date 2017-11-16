1fd0b16c28c9648fd62118a59ec577c382babd9f
Dillon Brout <djbrout@gmail.com>
2017-11-16 02:16:21 -0500

    need to get ra and dec delta for each night and append to bigdata

--- runsextractormasking.py
+++ runsextractormasking.py
@@ -60,7 +60,7 @@ def run(imagefilename,weightfilename,survey='DES',index='',bigreturn=False):
         print line
     print '-'*100
     print out["table"]
-    print out.__dict__
+    print out.keys()
 
 
 
