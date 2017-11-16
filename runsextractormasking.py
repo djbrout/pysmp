3f731e2a6c38659af9ea6c77788fa90c0398fe84
Dillon Brout <djbrout@gmail.com>
2017-11-16 15:42:31 -0500

    need to get ra and dec delta for each night and append to bigdata

--- runsextractormasking.py
+++ runsextractormasking.py
@@ -163,7 +163,7 @@ def run(imagefilename,weightfilename,survey='DES',index='',bigreturn=False):
 
 
     from scipy import fftpack
-    F1 = fftpack.fft2(im.astype(float))
+    F1 = fftpack.fft2(im[np.isfinite(im)].astype(float))
     F2 = fftpack.fftshift(F1)
     psd2D = np.abs(F2) ** 2
     psd1D = azimuthalAverage(psd2D)
