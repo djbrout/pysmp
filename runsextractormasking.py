6ba249f7a68778192086a61ead1f653d5384282e
Dillon Brout <djbrout@gmail.com>
2017-11-16 02:42:14 -0500

    need to get ra and dec delta for each night and append to bigdata

--- runsextractormasking.py
+++ runsextractormasking.py
@@ -77,6 +77,7 @@ def run(imagefilename,weightfilename,survey='DES',index='',bigreturn=False):
     wgt[wgt<1e-6] = 0.
     wgt[wgt>0] = 1.
     fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+    import numpy as np
     ax.imshow(np.log10(im*wgt), cmap="Greys")
     from matplotlib.patches import Ellipse
     ells = [Ellipse(xy=(x,y),
