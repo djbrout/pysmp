36d9fd74a738bfbee86a17d4b94757dae9dafc13
Dillon Brout <djbrout@gmail.com>
2018-02-15 10:30:37 -0500

    need to get ra and dec delta for each night and append to bigdata

--- batch_submission_scripts/looptest.py
+++ batch_submission_scripts/looptest.py
@@ -5,7 +5,7 @@ import time
 
 #allindexes = np.arange(0,2500)
 #18820
-allindexes = np.arange(22500,25000)
+allindexes = np.arange(25000,27500)
 #now up to 8000
 
 #allindexes = np.arange(362,4089,1)
